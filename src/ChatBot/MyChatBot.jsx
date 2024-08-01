332330c8352abbfecc605a5cca27a5242fd736a4
dspwebstudiomx <dspwebstudio@gmail.com>
2024-08-01 13:28:44 -0600

    "Refactored MyChatBot component: removed useState, extracted styles and settings to separate files, and updated chatbot flow and UI"

--- src/ChatBot/MyChatBot.jsx
+++ src/ChatBot/MyChatBot.jsx
@@ -1,156 +1,71 @@
-import { useState } from 'react';
 import ChatBot from 'react-chatbotify';
-import icon from '../assets/images/logos/horizontal/logo-blanco.png'
+import { styles } from './styles'
+import { settings } from './settings'
+
 const MyChatBot = () => {
-
-
-	// settings
-	const settings = {
-		isOpen: false,
-
-		emoji: {
-			disabled: 'false'
-		},
-		general: {
-			primaryColor: '#42b0c5',
-			secondaryColor: '#491d8d',
-			fontFamily: 'Arial, sans-serif',
-			embedded: false,
-		},
-		header: {
-			title: 'Easylav ChatBot'
-		},
-		audio: {
-			disabled: true,
-		},
-		chatHistory: {
-			disabled: true,
-			storageKey: 'concepts_settings'
-		},
-		chatButton: {
-			icon: icon
-		},
-		tooltip: {
-			text: '¿Tienes dudas?',
-		},
-		chatInput: {
-			enabledPlaceholderText: 'Escribe tu mensaje...'
-		},
-		botBubble: {
-			showAvatar: true
-		},
-		fileAttachment: {
-			disabled: true
-		},
-		footer: {
-			text: 'Lavanderías EASYLAV'
-		}
-	}
-	// styles here
-	const styles = {
-		headerStyle: {
-			background: '#55C3B9',
-			color: '#ffffff',
-			letterSpacing: '1px',
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'between',
-			fontSize: '21px'
-		},
-		chatWindowStyle: {
-			backgroundColor: '#f2f2f2',
-		},
-		chatButtonStyle: {
-			backgroundColor: '#622a77',
-			fontSize: '14px'
-		},
-		icon: {
-			width: '32px'
-		},
-		tooltipStyle: {
-			backgroundColor: '#55C3B9',
-			border: '1px solid #f2f2f2'
-		},
-		botBubbleStyle: {
-			backgroundColor: '#622a77',
-			fontSize: '18px'
-		},
-		userBubbleStyle: {
-			backgroundColor: '#55C3B9'
-		},
-		bodyStyle: {
-			padding: '1.1em'
-		}
-	}
-	const [form, setForm] = useState({});
-	const formStyle = {
-		marginTop: 10,
-		marginLeft: 20,
-		border: '1px solid #491d8d',
-		padding: 10,
-		borderRadius: 5,
-		maxWidth: 300
-	}
-
-
+	const helpOptions = ["Quickstart", "API Docs", "Examples", "Github", "Discord"];
 	const flow = {
 		start: {
-			message: 'Hola, Bienvenido! 😎 ¿Cuál es tu nombre?',
-			function: (params) => setForm({ ...form, name: params.userInput }),
-			path: 'preguntar_opciones'
+			message: "Hello, I am Tan Jin 👋! Welcome to React ChatBotify, I'm excited that you are using our " +
+				"chatbot 😊!",
+			transition: { duration: 1000 },
+			path: "show_options"
 		},
-		preguntar_duda: {
-			message: (params) => `Mucho gusto ${params.userInput}, en que puedo ayudarte?`,
-			function: (params) => setForm({ ...form, name: params.preguntar_opciones }),
-			path: 'preguntar_opciones'
+		show_options: {
+			message: "It looks like you have not set up a conversation flow yet. No worries! Here are a few helpful " +
+				"things you can check out to get started:",
+			options: helpOptions,
+			path: "process_options"
 		},
-		ask_pet: {
-			message: 'Do you own any pets?',
-			options: ['Yes', 'No'],
+		prompt_again: {
+			message: "Do you need any other help?",
+			options: helpOptions,
+			path: "process_options"
+		},
+		unknown_input: {
+			message: "Sorry, I do not understand your message 😢! If you require further assistance, you may click on " +
+				"the Github option and open an issue there or visit our discord.",
+			options: helpOptions,
+			path: "process_options"
+		},
+		process_options: {
+			transition: { duration: 0 },
 			chatDisabled: true,
-			function: (params) => setForm({ ...form, pet_ownership: params.userInput }),
-			path: 'ask_choice'
+			path: async (params) => {
+				let link = "";
+				switch (params.userInput) {
+					case "Quickstart":
+						link = "https://react-chatbotify.com/docs/introduction/quickstart/";
+						break;
+					case "API Docs":
+						link = "https://react-chatbotify.com/docs/api/settings";
+						break;
+					case "Examples":
+						link = "https://react-chatbotify.com/docs/examples/basic_form";
+						break;
+					case "Github":
+						link = "https://github.com/tjtanjin/react-chatbotify/";
+						break;
+					case "Discord":
+						link = "https://discord.gg/6R4DK4G5Zh";
+						break;
+					default:
+						return "unknown_input";
+				}
+				await params.injectMessage("Sit tight! I'll send you right there!");
+				setTimeout(() => {
+					window.open(link);
+				}, 1000)
+				return "repeat"
+			},
 		},
-		preguntar_opciones: {
-			message: (params) => `Mucho gusto ${params.userInput}, en que puedo ayudarte?`,
-			options: [
-				{ value: 1, label: 'Number 1', path: '1' },
-				{ value: 2, label: 'Number 2', path: '2' },
-				{ value: 3, label: 'Number 3', path: '3' },
-			],
-			chatDisabled: true,
-			function: (params) => setForm({ ...form, pet_choices: params.userInput }),
-			path: 'ask_work_days'
-		},
-		ask_work_days: {
-			message: 'How many days can you work per week?',
-			function: (params) => setForm({ ...form, num_work_days: params.userInput }),
-			path: 'end'
-		},
-		end: {
-			message: 'Thank you for your interest, we will get back to you shortly!',
-			component: (
-				<div style={formStyle}>
-					<p>Name: {form.name}</p>
-					<p>Age: {form.age}</p>
-					<p>Pet Ownership: {form.pet_ownership}</p>
-					<p>Pet Choices: {form.pet_choices}</p>
-					<p>Num Work Days: {form.num_work_days}</p>
-				</div>
-			),
-			options: ['New Application'],
-			chatDisabled: true,
-			path: 'start'
+		repeat: {
+			transition: { duration: 3000 },
+			path: "prompt_again"
 		},
 	}
 	return (
-		<div className='fixed bottom-24 right-20 z-50'>
-			<ChatBot
-				styles={styles}
-				settings={settings}
-				flow={flow}
-			/>
-		</div>
+		<ChatBot settings={settings} styles={styles} flow={flow} />
 	);
-};
+}
 export default MyChatBot
---
+++ src/ChatBot/settings.js
@@ -0,0 +1,43 @@
+import icon from "../assets/images/logos/horizontal/logo-blanco.png";
+
+export const settings = {
+  isOpen: false,
+
+  emoji: {
+    disabled: "false",
+  },
+  general: {
+    primaryColor: "#42b0c5",
+    secondaryColor: "#491d8d",
+    fontFamily: "Arial, sans-serif",
+    embedded: false,
+  },
+  header: {
+    title: "Easylav ChatBot",
+  },
+  audio: {
+    disabled: true,
+  },
+  chatHistory: {
+    disabled: true,
+    storageKey: "concepts_settings",
+  },
+  chatButton: {
+    icon: icon,
+  },
+  tooltip: {
+    text: "¿Tienes dudas?",
+  },
+  chatInput: {
+    enabledPlaceholderText: "Escribe tu mensaje...",
+  },
+  botBubble: {
+    showAvatar: true,
+  },
+  fileAttachment: {
+    disabled: true,
+  },
+  footer: {
+    text: "Lavanderías EASYLAV",
+  },
+};
