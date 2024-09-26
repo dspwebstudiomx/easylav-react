31508a332cecdcfd3ebb71d6ef0a20a6c91b4e30
dspwebstudiomx <dspwebstudio@gmail.com>
2024-09-26 17:54:36 -0600

    "Updated Footer component: changed 'justify-center' to 'justify-between' and 'text-light' to 'text-secondary_dark' in footer links"

--- src/components/templates/footer/Footer.jsx
+++ src/components/templates/footer/Footer.jsx
@@ -69,14 +69,14 @@ export default function Footer() {
           </Container>
         </div>
         <div className="bg-primary py-6 text-light">
-          <div className="mx-auto flex w-[80vw] flex-col justify-center gap-4 text-center text-sm font-semibold sm:w-[65vw] sm:flex-row sm:gap-20 sm:text-left xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+          <div className="mx-auto flex w-[80vw] flex-col justify-between gap-4 text-center text-sm font-semibold sm:w-[65vw] sm:flex-row sm:gap-20 sm:text-left xl:max-w-screen-xl 2xl:max-w-screen-2xl">
             <h3>Derechos Reservados 2024</h3>
             <h3 className="flex flex-col lg:flex-row">
               <span>Diseñado y Desarrollado por:</span>
               <a
                 href="https://dspwebstudio.com"
                 target="_blank"
-                className="ml-2 font-semibold text-light"
+                className="ml-2 font-semibold text-secondary_dark"
               >
                 dspwebstudio.com
               </a>
