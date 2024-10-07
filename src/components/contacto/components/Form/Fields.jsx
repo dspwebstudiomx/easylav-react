d3015aa8d4fab6cd406375626e101f2362efc168
dspwebstudiomx <dspwebstudio@gmail.com>
2024-10-06 19:34:17 -0600

    changed Fields width

--- src/components/contacto/components/Form/Fields.jsx
+++ src/components/contacto/components/Form/Fields.jsx
@@ -3,80 +3,89 @@ import PropTypes from 'prop-types';
 
 const Fields = ({ errors, touched }) => {
   return (
-    <div className="flex flex-wrap justify-between gap-3">
-      {/* Nombre Completo */}
-      <div className="flex flex-col xl:w-[50%] w-full">
-        <label htmlFor="user_name" className="mb-2">
-          Nombre Completo<span className="text-required ml-1">*</span>
-        </label>
-        <Field
-          id="user_name"
-          name="user_name"
-          className="rounded-md border-2 border-primary p-2 outline-none  bg-light text-dark"
-          type="text"
-          placeholder="Nombre Completo"
-          required
-        />
-        {touched.user_name && errors.user_name && (
-          <p className="mt-2 text-required text-xs">
-            * <span className="text-dark">{errors.user_name}</span>
-          </p>
-        )}
-      </div>
-      {/* Nombre Completo */}
-
-      {/* Ciudad */}
-      <div className="flex flex-col xl:w-[45%] w-full">
-        <label htmlFor="user_city" className="mb-2">
-          Ciudad<span className="text-required ml-1">*</span>
-        </label>
-        <Field
-          className="rounded-md border-2 border-primary p-2 outline-none bg-light text-dark"
-          id="user_city"
-          name="user_city"
-          placeholder="Ciudad"
-          required
-        />
-        {touched.user_city && errors.user_city && (
-          <p className="mt-2 text-required text-xs">
-            * <span className="text-dark">{errors.user_city}</span>
-          </p>
-        )}
-      </div>
-      {/* Ciudad */}
-
-      {/* Correo Electrónico */}
-      <div className='flex flex-wrap justify-between gap-3'>
-        <div className='flex flex-col xl:w-[50%] w-full'>
-          <label htmlFor='user_email' className='mb-2'>Correo Electrónico<span className='text-required ml-1'>*</span></label>
+    <div className="flex flex-col justify-between gap-3">
+      <div className='flex flex-col sm:flex-row gap-3'>
+        {/* Nombre Completo */}
+        <div className="flex flex-col sm:w-[50%] w-full">
+          <label htmlFor="user_name" className="mb-2">
+            Nombre Completo<span className="text-required ml-1">*</span>
+          </label>
           <Field
-            className='rounded-md border-2 border-primary p-2 outline-none  bg-light text-dark'
-            id='user_email'
-            name='user_email'
-            type='email'
-            placeholder='Correo electrónico'
+            id="user_name"
+            name="user_name"
+            className="rounded-md border-2 border-primary p-2 outline-none  bg-light text-dark"
+            type="text"
+            placeholder="Nombre Completo"
             required
           />
-          {touched.user_email && errors.user_email && <p className='mt-2 text-required text-xs'>* <span className='text-dark'>{errors.user_email}</span></p>}
-
+          {touched.user_name && errors.user_name && (
+            <p className="mt-2 text-required text-xs">
+              * <span className="text-dark">{errors.user_name}</span>
+            </p>
+          )}
         </div>
-        <div className='flex flex-col xl:w-[45%] w-full'>
-          <label htmlFor='user_phone' className='mb-2'>Número Telefónico<span className='text-required ml-1'>*</span></label>
+        {/* Nombre Completo */}
+
+        {/* Ciudad */}
+        <div className="flex flex-col sm:w-[45%] w-full">
+          <label htmlFor="user_city" className="mb-2">
+            Ciudad<span className="text-required ml-1">*</span>
+          </label>
           <Field
-            className='rounded-md border-2 border-primary p-2 outline-none bg-light text-dark'
-            type='text'
-            name='user_phone'
-            id='user_phone'
-            placeholder='Número telefónico'
+            className="rounded-md border-2 border-primary p-2 outline-none bg-light text-dark"
+            id="user_city"
+            name="user_city"
+            placeholder="Ciudad"
             required
           />
-          {touched.user_phone && errors.user_phone && <p className='mt-2 text-required text-xs'>* <span className='text-dark'>{errors.user_phone}</span></p>}
-
+          {touched.user_city && errors.user_city && (
+            <p className="mt-2 text-required text-xs">
+              * <span className="text-dark">{errors.user_city}</span>
+            </p>
+          )}
         </div>
+        {/* Ciudad */}
       </div>
-      {/* Correo Electrónico */}
 
-      {/* Número Telefónico */}
+      <div className=''>
+        {/* Correo Electrónico */}
+        <div className='flex justify-between gap-3'>
+          <div className='flex flex-col sm:w-[45%] w-full'>
+            <label htmlFor='user_email' className='mb-2'>Correo Electrónico<span className='text-required ml-1'>*</span></label>
+            <Field
+              className='rounded-md border-2 border-primary p-2 outline-none  bg-light text-dark'
+              id='user_email'
+              name='user_email'
+              type='email'
+              placeholder='Correo electrónico'
+              required
+            />
+            {touched.user_email && errors.user_email && <p className='mt-2 text-required text-xs'>* <span className='text-dark'>{errors.user_email}</span></p>}
+
+          </div>
+          {/* Correo Electrónico */}
+
+          {/* Número Telefónico */}
+          <div className='flex flex-col sm:w-[50%] w-full'>
+            <label htmlFor='user_phone' className='mb-2'>Número Telefónico<span className='text-required ml-1'>*</span></label>
+            <Field
+              className='rounded-md border-2 border-primary p-2 outline-none bg-light text-dark'
+              type='text'
+              name='user_phone'
+              id='user_phone'
+              placeholder='Número telefónico'
+              required
+            />
+            {touched.user_phone && errors.user_phone && <p className='mt-2 text-required text-xs'>* <span className='text-dark'>{errors.user_phone}</span></p>}
+
+          </div>
+        </div>
+        {/* Número Telefónico */}
+
+
+        {/* Número Telefónico */}
+      </div>
+      {/* Mensaje */}
       <div className='flex flex-col w-full'>
         <label htmlFor='message' className='mb-2'>Mensaje<span className='text-required ml-1'>*</span></label>
         <textarea
@@ -87,9 +96,9 @@ const Fields = ({ errors, touched }) => {
           required
         />
         {touched.message && errors.message && <p className='mt-2 text-blue-400 text-xs'>* {errors.message}</p>}
-
       </div>
-      {/* Número Telefónico */}
+      {/* Mensaje */}
+
     </div>
   );
 };
