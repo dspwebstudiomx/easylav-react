72662f21dc2fde56690f8690ec7c155b056c5635
dspwebstudiomx <dspwebstudio@gmail.com>
2024-12-08 18:35:01 -0600

    sucursalcard

--- src/pages/sucursales/components/CityCardGenerator.jsx
+++ src/pages/sucursales/components/CityCardGenerator.jsx
@@ -6,27 +6,6 @@ const CityCardGenerator = ({ city }) => {
 
   const ciudad = localservices.filter(localservice => localservice.city === city)
   ciudad.sort((a, b) => a.title.localeCompare(b.title));
-
-  // Obtenemos la hora actual
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
-  const currentMinute = currentTime.getMinutes();
-
-  // Definimos el horario de servicio
-  const openHours = localservices.map((sucursal) => sucursal.openHour);
-  const openMinutes = localservices.map((sucursal) => sucursal.openMinute);
-  const closeHours = localservices.map((sucursal) => sucursal.closeHour);
-  const closeMinutes = localservices.map((sucursal) => sucursal.closeMinute);
-
-  // Comparamos la hora actual con el horario de servicio
-  const isOpen = localservices.map((sucursal, index) => {
-    const openTime = openHours[index] * 60 + openMinutes[index];
-    const closeTime = closeHours[index] * 60 + closeMinutes[index];
-    const currentTimeInMinutes = currentHour * 60 + currentMinute; // Convertir a minutos
-
-    return currentTimeInMinutes >= openTime && currentTimeInMinutes < closeTime;
-  });
-
   return (
     <article className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-12 items-center justify-center'>
       {
@@ -47,7 +26,6 @@ const CityCardGenerator = ({ city }) => {
             closeHour={localservice.closeHour}
             openMinute={localservice.openMinute}
             closeMinute={localservice.closeMinute}
-            isOpen={isOpen}
           />
         )
       }
