cf94250b271dcec44caeba79fd9130f9e15feb65
dspwebstudiomx <dspwebstudio@gmail.com>
2025-01-13 17:08:09 -0600

    `Updated FranquiciasTarjetas and PaquetesFranquicias components with minor styling and layout changes`

--- src/pages/franquicias/FranquiciasTarjetas.jsx
+++ src/pages/franquicias/FranquiciasTarjetas.jsx
@@ -1,6 +1,6 @@
 // Importaciones
 import { VistaDentroLavanderia_640 as image } from 'assets';
-import { Accordion, BorderLeft, ButtonContainer, ButtonPrimary, ButtonSecondary, ContactFormFranquicias, Container, Footer, ImageResponsive, Modal, Navbar, ScrollToTopButton, SEOFriendly, Spacing, TitleH2 } from "components";
+import { Accordion, BorderLeft, ButtonContainer, ButtonPrimary, ButtonSecondary, ContactFormFranquicias, Container, Footer, ImageResponsive, Modal, Navbar, ScrollToTopButton, Section, SEOFriendly, Spacing, TitleH2 } from "components";
 import { franquiciaDescripciones, franquiciasImagenes } from "data";
 import { useState } from 'react';
 import { FaHome, FaInfoCircle } from "react-icons/fa";
@@ -66,7 +66,7 @@ const FranquiciasTarjetas = () => {
         ogType="website"
       />
       <Navbar />
-      <div className='bg-light mt-[100px] sm:mt-0 pb-[100px]'>
+      <div className='bg-light mt-[100px] sm:mt-0 xl:mt-[100px] b-[100px]'>
         {/* Banner */}
         <section className="bg-secondary_dark text-light px-8 dark:bg-primary_dark">
           <div className="mx-auto max-w-screen-2xl px-4 py-32 lg:flex lg:h-2xl lg:items-center">
@@ -102,70 +102,72 @@ const FranquiciasTarjetas = () => {
         {/* Paquetes de Franquicias */}
 
         {/* Contenido */}
-        <Container className="flex flex-col sm:gap-12" id='Sobre-franquicias'>
-          {/* Title */}
-          <div className="w-full px-4 sm:px-0">
-            <div className="mx-auto mb-[60px] lg:mb-0 flex  flex-col gap-4">
-              <BorderLeft>
-                <TitleH2 className={styles} textColor="text-dark dark:text-light w-full">
-                  SOBRE NUESTRAS FRANQUICIAS
-                </TitleH2>
-              </BorderLeft>
-              <h3 className='text-xl font-semibold sm:text-2xl mt-8 text-dark dark:text-light'>Visita cada punto para ver la información relacionada.</h3>
+        <Section className="flex flex-col sm:gap-12" id='Sobre-franquicias'>
+          <Container>
+            {/* Title */}
+            <div className="w-full px-4 sm:px-0">
+              <div className="mx-auto mb-[60px] lg:mb-0 flex  flex-col gap-4">
+                <BorderLeft>
+                  <TitleH2 className={styles} textColor="text-dark dark:text-light w-full">
+                    SOBRE NUESTRAS FRANQUICIAS
+                  </TitleH2>
+                </BorderLeft>
+                <h3 className='text-xl font-semibold sm:text-2xl mt-8 text-dark dark:text-light'>Visita cada punto para ver la información relacionada.</h3>
+              </div>
             </div>
-          </div>
-          {/* Title */}
-          <div className=''>
-            {/* Questions */}
-            <ul className="grid sm:grid-cols-2 gap-8">
-              {franquiciaDescripciones.map(description => {
-                return (
-                  <li key={description.id} id={`descripcion-${description.id}`} className=" text-dark">
-                    <Accordion
-                      height='h-[120px]'
-                      title={description.title}
-                      description={description.description}
-                    />
-                  </li>
+            {/* Title */}
+            <div className=''>
+              {/* Questions */}
+              <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
+                {franquiciaDescripciones.map(description => {
+                  return (
+                    <li key={description.id} id={`descripcion-${description.id}`} className=" text-dark">
+                      <Accordion
+                        height='h-[120px]'
+                        title={description.title}
+                        description={description.description}
+                      />
+                    </li>
+                  )
+                })}
+              </ul>
+              <ButtonContainer position={"justify-center sm:justify-center items-center my-20"}          >
+                <ButtonSecondary href={'/sucursales'} title={'Ver Sucursales'} icon={<FaRegBuilding />} width={'min-w-[240px] sm:w-[140px]'} name='ver sucursales' />
+              </ButtonContainer>
+              {/* Questions */}
+
+              <div id="franquicias-columma-2" className={`${FRANQUICIAS_UI.COLUMNA_2.DISPLAY} ${FRANQUICIAS_UI.COLUMNA_2.HEIGHT} ${FRANQUICIAS_UI.COLUMNA_2.MARGIN} ${FRANQUICIAS_UI.COLUMNA_2.PADDING} ${FRANQUICIAS_UI.COLUMNA_2.WIDTH}`}
+              >
+                {franquiciasImagenes.map(imagen => {
+                  return (
+                    <div key={imagen.id} className="h-auto rotate-[0deg]">
+                      <ImageResponsive
+                        src={imagen.image_640}
+                        className={`${FRANQUICIAS_UI.IMAGE.WIDTH}`}
+                        imageAlt={imagen.image_640}
+                        image_640={imagen.image_640}
+                        image_1024={imagen.image_640}
+                        image_1920={imagen.image_640}
+                        width={380}
+                      />
+                    </div>
+                  )
+                }
                 )
-              })}
-            </ul>
-            <ButtonContainer position={"justify-center sm:justify-center items-center my-20"}          >
-              <ButtonSecondary href={'/sucursales'} title={'Ver Sucursales'} icon={<FaRegBuilding />} width={'min-w-[240px] sm:w-[140px]'} name='ver sucursales' />
+                }
+              </div>
+            </div>
+            <ButtonContainer position={'items-center sm:justify-center my-8'}>
+              <ButtonPrimary href={'/#inicio'} title={'Regresar a inicio'} icon={<FaHome />} width={'min-w-[240px]'} name='Regresar a inicio' />
+              <ButtonSecondary title={"Solicita más información"}
+                width={"w-[340px]"}
+                onClick={handleModalToggle}
+                icon={<FaInfoCircle />}
+                distance='mx-1' />
+
             </ButtonContainer>
-            {/* Questions */}
-
-            <div id="franquicias-columma-2" className={`${FRANQUICIAS_UI.COLUMNA_2.DISPLAY} ${FRANQUICIAS_UI.COLUMNA_2.HEIGHT} ${FRANQUICIAS_UI.COLUMNA_2.MARGIN} ${FRANQUICIAS_UI.COLUMNA_2.PADDING} ${FRANQUICIAS_UI.COLUMNA_2.WIDTH}`}
-            >
-              {franquiciasImagenes.map(imagen => {
-                return (
-                  <div key={imagen.id} className="h-auto rotate-[0deg]">
-                    <ImageResponsive
-                      src={imagen.image_640}
-                      className={`${FRANQUICIAS_UI.IMAGE.WIDTH}`}
-                      imageAlt={imagen.image_640}
-                      image_640={imagen.image_640}
-                      image_1024={imagen.image_640}
-                      image_1920={imagen.image_640}
-                      width={380}
-                    />
-                  </div>
-                )
-              }
-              )
-              }
-            </div>
-          </div>
-          <ButtonContainer position={'items-center sm:justify-center my-8'}>
-            <ButtonPrimary href={'/#inicio'} title={'Regresar a inicio'} icon={<FaHome />} width={'min-w-[240px]'} name='Regresar a inicio' />
-            <ButtonSecondary title={"Solicita más información"}
-              width={"w-[340px]"}
-              onClick={handleModalToggle}
-              icon={<FaInfoCircle />}
-              distance='mx-1' />
-
-          </ButtonContainer>
-        </Container>
+          </Container>
+        </Section>
         {/* Contenido */}
 
       </div>
--- src/pages/franquicias/PaquetesFranquicias.jsx
+++ src/pages/franquicias/PaquetesFranquicias.jsx
@@ -1,5 +1,5 @@
 // Importaciones
-import { ButtonSecondary } from "components"
+import { ButtonSecondary, Container, Section } from "components"
 import { paquetesFranquicias } from "data"
 import { useState } from "react"
 import { FaCheck } from "react-icons/fa6"
@@ -7,7 +7,7 @@ import { FaCheck } from "react-icons/fa6"
 // Estilos
 const PAQUETES_FRANQUICIAS_UI = {
   PADDING: "px-8 py-12 sm:py-12 md:px-20 lg:p-24 ",
-  MARGIN: "mx-auto",
+  MARGIN: "m-auto",
   BACKGROUND: "bg-light dark:bg-dark",
   TEXT_COLOR: "text-dark"
 }
@@ -24,49 +24,51 @@ const PaquetesFranquicias = () => {
     document.body.style.overflow = !showModal && "hidden";
   };
   return (
-    <section className={styles}>
-      <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 xl:grid-cols-4 md:gap-12 max-w-screen-2xl mx-auto">
+    <Section className={styles}>
+      <Container>
+        <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 xl:grid-cols-4 md:gap-12 max-w-screen-2xl mx-auto">
 
-        {paquetesFranquicias.map(paquete => {
-          return (
-            <li key={paquete.id} className="divide-y divide-primary_dark rounded-2xl border-primary_dark border-4 shadow-2xl hover:-translate-y-1 hover:scale-105 dark:bg-light">
+          {paquetesFranquicias.map(paquete => {
+            return (
+              <li key={paquete.id} className="divide-y divide-primary_dark rounded-2xl border-primary_dark border-4 shadow-2xl hover:-translate-y-1 hover:scale-105 dark:bg-light">
 
-              {/* Primera Parte */}
-              <div className="p-8 flex flex-col gap-6">
-                <h2 className="text-3xl text-dark uppercase font-bold text-left">
-                  {paquete.nombre}
-                  <span className="sr-only">Plan</span>
-                </h2>
-                <p className="mt-2 text-dark-700">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <ButtonSecondary title={"Contactar"}
-                  width={"w-full 2xl:w-2/3 mx-auto"}
-                  onClick={handleModalToggle}
-                  icon={''}
-                  distance='mx-1' />
-              </div>
-              {/* Primera Parte */}
+                {/* Primera Parte */}
+                <div className="p-8 flex flex-col gap-6">
+                  <h2 className="text-3xl text-dark uppercase font-bold text-left">
+                    {paquete.nombre}
+                    <span className="sr-only">Plan</span>
+                  </h2>
+                  <p className="mt-2 text-dark-700">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <ButtonSecondary title={"Contactar"}
+                    width={"w-full 2xl:w-2/3 mx-auto"}
+                    onClick={handleModalToggle}
+                    icon={''}
+                    distance='mx-1' />
+                </div>
+                {/* Primera Parte */}
 
-              {/* Segunda Parte */}
-              <div className="p-8">
-                <p className="text-lg font-medium text-gray-900 sm:text-xl">Que incluye <span className='text-secondary_dark'>:</span></p>
+                {/* Segunda Parte */}
+                <div className="p-8">
+                  <p className="text-lg font-medium text-gray-900 sm:text-xl">Que incluye <span className='text-secondary_dark'>:</span></p>
 
-                <ul className="mt-2 space-y-2 sm:mt-4" id={`lista-caracteristica-${paquete.nombre}`}>
-                  {paquete.caracteristicas.map((caracteristica, index) => (
-                    <li key={index} className="text-gray-700 flex gap-4 ">
-                      <FaCheck className='text-secondary_dark' />
-                      <span>{caracteristica}</span></li>
-                  ))}
-                </ul>
-              </div>
-              {/* Segunda Parte */}
+                  <ul className="mt-2 space-y-2 sm:mt-4" id={`lista-caracteristica-${paquete.nombre}`}>
+                    {paquete.caracteristicas.map((caracteristica, index) => (
+                      <li key={index} className="text-gray-700 flex gap-4 ">
+                        <FaCheck className='text-secondary_dark' />
+                        <span>{caracteristica}</span></li>
+                    ))}
+                  </ul>
+                </div>
+                {/* Segunda Parte */}
 
-            </li>
-          )
-        })}
+              </li>
+            )
+          })}
 
 
-      </ul>
-    </section>
+        </ul>
+      </Container>
+    </Section>
   )
 }
 
