bbba5bb9d034b2c6b2e2ad230dd1ba751b4d1e20
dspwebstudiomx <dspwebstudio@gmail.com>
2025-02-02 09:13:26 -0600

    `Refactor Service component and ServicesGenerator component to update class names and element types`

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -2,9 +2,9 @@ import { TitleH3 } from 'components';
 import PropTypes from 'prop-types';
 
 const SERVICE_UI = {
-  LI: {
+  ELEMENT: {
     CLASSNAME:
-      "flex flex-col gap-8 items-center justify-center shadow-2xl rounded-xl bg-light p-8 w-[260px] xl:w-[200px] hover:scale-110 duration-300",
+      "flex flex-col gap-8 items-center justify-center shadow-2xl rounded-xl bg-ELEMENTght p-8 w-[260px] xl:w-[200px] hover:scale-110 duration-300",
     DIV_VIDEO: "mx-auto w-32 sm:w-24 md:w-40",
     DIV_TITLEH3: "mx-auto text-primary",
   },
@@ -14,11 +14,11 @@ const SERVICE_UI = {
 
 const Service = ({ service }) => {
   return (
-    <li id={service.idLink} className={SERVICE_UI.LI.CLASSNAME}>
-      <div className={SERVICE_UI.LI.DIV_VIDEO}>
+    <li id={service.idLink} className={SERVICE_UI.ELEMENT.CLASSNAME}>
+      <div className={SERVICE_UI.ELEMENT.DIV_VIDEO}>
         <img className={SERVICE_UI.IMAGE} src={service.image} title={service.alt} alt={service.alt} />
       </div>
-      <div className={SERVICE_UI.LI.DIV_TITLEH3}>
+      <div className={SERVICE_UI.ELEMENT.DIV_TITLEH3}>
         <TitleH3>{service.title}</TitleH3>
       </div>
       <p className={SERVICE_UI.PARAGRAPH}>
--- src/pages/inicio/nuestrosServicios/ServicesGenerator.jsx
+++ src/pages/inicio/nuestrosServicios/ServicesGenerator.jsx
@@ -12,19 +12,19 @@ import { services } from 'data';
 
 const SERVICES_UI = {
   DIV: "w-full",
-  UL: "flex flex-wrap gap-12",
+  LIST: "flex flex-wrap gap-12 items-center justify-center",
 }
 
 // Estilos
 const SERVICES_PROPS = {
   ID: "nuestros-servicios",
 };
-
+8
 // Estructura
 const ServicesGenerator = () => {
   return (
     <div id={SERVICES_PROPS.ID} className={SERVICES_UI.DIV}>
-      <ul className={SERVICES_UI.UL}>
+      <ul className={SERVICES_UI.LIST}>
         {services.map((service, key) => {
           return (
             <Service key={key} service={service} />
