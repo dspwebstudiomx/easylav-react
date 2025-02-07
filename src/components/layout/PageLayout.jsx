2a391c238c8d5f822b4e77573216b9b260b5c4ea
dspwebstudiomx <dspwebstudio@gmail.com>
2025-02-07 13:04:21 -0600

    `Update PageLayout main element's margin-top classes`

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -6,7 +6,7 @@ const PageLayout = ({ children, id }) => {
   return (
     <>
       <Header />
-      <main className="mt-[100px] sm:mt-[60px] xl:mt-[100px]">
+      <main className="mt-[100px] sm:mt-[0px] xl:mt-[100px]">
         <Section backgroundColor={'dark:bg-dark bg-light'} height={'md:min-h-[42vh] 2xl:min-h-[49svh]'} className={'dark:text-light flex flex-col gap-6 text-dark'}>
           <Container id={id}>
             {children}
