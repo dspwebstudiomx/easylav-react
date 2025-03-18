a5c12fca42f5042c84e0aac69f3993fb5e2bdc1c
dspwebstudiomx <dspwebstudio@gmail.com>
2025-03-18 12:21:07 -0600

    Refactors Service component for improved readability and structure; adjusts styles for better layout and consistency in modal and service display.

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -6,68 +6,86 @@
 */
 
 // Importaciones
-import { ButtonContainer, ButtonSecondary, Modal, Paragraph, TitleH2, TitleH3 } from 'components';
-import { useShowModal } from 'hooks';
-import PropTypes from 'prop-types';
-import { FaXmark } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
+import {
+  ButtonContainer,
+  ButtonSecondary,
+  Modal,
+  Paragraph,
+  TitleH2,
+  TitleH3,
+} from "components";
+import { useShowModal } from "hooks";
+import PropTypes from "prop-types";
+import { FaXmark } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 // Estilos
 const SERVICE_UI = {
   ELEMENT: {
-    CLASSNAME:
-      `flex flex-col gap-4 items-center justify-center shadow-2xl rounded-xl p-8 w-[100%] md:w-[180px] xl:w-[200px]  bg-light dark:border-4 dark:border-primary mx-auto`,
-    DIV_VIDEO: "mx-auto w-32 sm:w-22 md:w-40",
+    CLASSNAME: `flex flex-col gap-4 items-center justify-center shadow-2xl rounded-xl p-6 w-[100%] md:w-[160px] bg-light dark:border-4 dark:border-primary mx-auto`,
+    DIV_VIDEO: "mx-auto w-32 sm:w-22 md:w-32",
     DIV_TITLEH3: "mx-auto text-primary",
   },
-  IMAGE: "p-2 w-28 mx-auto text-center",
+  IMAGE: "p-0 w-20 mx-auto text-center",
   PARAGRAPH: "text-dark text-[16px] hidden",
 };
 
 // Estructura
 const Service = ({ service }) => {
-  const { showModal, setShowModal } = useShowModal()
+  const { showModal, setShowModal } = useShowModal();
 
   return (
     <li id={service.idLink} className={SERVICE_UI.ELEMENT.CLASSNAME}>
       <Link onClick={() => setShowModal(true)}>
         <div className={SERVICE_UI.ELEMENT.DIV_VIDEO}>
-          <img className={SERVICE_UI.IMAGE} src={service.image} title={service.alt} alt={service.alt} />
+          <img
+            className={SERVICE_UI.IMAGE}
+            src={service.image}
+            title={service.alt}
+            alt={service.alt}
+          />
         </div>
         <div className={`${SERVICE_UI.ELEMENT.DIV_TITLEH3} my-2`}>
-          <TitleH3 justify="justify-center" color="text-dark">{service.title}</TitleH3>
+          <TitleH3 justify="justify-center" color="text-dark">
+            {service.title}
+          </TitleH3>
         </div>
-        <p className={SERVICE_UI.PARAGRAPH}>
-          {service.description}
-        </p>
+        <p className={SERVICE_UI.PARAGRAPH}>{service.description}</p>
       </Link>
       {/* Modal */}
       {showModal && (
-        <Modal width='w-[90vw] md:w-[60vw] lg:w-[60vw]' height='h-auto'>
-          <div id="modal-servicio" className="z-40 mx-auto flex flex-col gap-8 rounded-xl border-4 border-primary bg-light p-8 sm:p-12 h-full justify-center">
-            <button id="button-close" onClick={() => setShowModal(false)} className=''>
-              <FaXmark
-                size={36}
-                className="z-30 ml-auto text-primary_dark z-50"
-              />
+        <Modal width="w-[100vw] md:w-[60vw] lg:w-[40vw]" height="h-auto">
+          <div
+            id="modal-servicio"
+            className="z-40 mx-auto flex h-full flex-col justify-center gap-8 overflow-scroll rounded-xl border-4 border-primary bg-light p-8 sm:p-12"
+          >
+            <button
+              id="button-close"
+              onClick={() => setShowModal(false)}
+              className=""
+            >
+              <FaXmark size={36} className="z-30 ml-auto text-primary_dark" />
             </button>
-            <div className='flex flex-col sm:flex-row justify-between'>
-              <div className='flex flex-col sm:flex-row gap-8 justify-start items-center'>
-                <img src={service.image} title={service.alt} alt={service.alt} width={80} />
-                <TitleH2 align='justify-center'>{service.title}</TitleH2>
+            <div className="flex flex-col justify-between sm:flex-row">
+              <div className="flex flex-col items-center justify-start gap-4 sm:flex-row">
+                <img
+                  src={service.image}
+                  title={service.alt}
+                  alt={service.alt}
+                  width={80}
+                />
+                <TitleH2 align="justify-center">{service.title}</TitleH2>
               </div>
             </div>
             <Paragraph>
-              <span className='text-dark mt-4'>
-                {service.description}
-              </span>
+              <span className="mt-4 text-dark">{service.description}</span>
             </Paragraph>
-            <ButtonContainer position='justify-center sm:justify-end items-center'>
+            <ButtonContainer position="justify-center sm:justify-end items-center">
               <ButtonSecondary
-                title='Conócenos'
-                href='/sucursales'
-                width='w-full md:w-[210px]'
-                name='botón conócenos'
+                title="Conócenos"
+                href="/sucursales"
+                width="w-full md:w-[210px]"
+                name="botón conócenos"
               />
             </ButtonContainer>
           </div>
@@ -75,8 +93,8 @@ const Service = ({ service }) => {
       )}
       {/* Modal */}
     </li>
-  )
-}
+  );
+};
 
 Service.propTypes = {
   service: PropTypes.shape({
