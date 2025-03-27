cc41a7aa59f1906648d6b456fb5b63d7c4095903
dspwebstudiomx <dspwebstudio@gmail.com>
2025-03-27 15:10:38 -0600

    Refactors HeroInicioVideo component for improved structure and clarity, adds documentation comments for better understanding

--- src/pages/inicio/components/HeroInicioVideo.jsx
+++ src/pages/inicio/components/HeroInicioVideo.jsx
@@ -1,56 +1,71 @@
+/*
+  HeroInicioVideo.jsx - Inicio - Sección
+  =====================================
+  Creado por : Daniel Pérez
+  Fecha: 2024-10-18
+  Descripción: Componente de video de fondo para la sección de inicio.
+  Este componente utiliza un video de fondo y una imagen de respaldo para mostrar un mensaje de bienvenida y un botón de llamada a la acción.
+  Modificado por: Daniel Pérez
+  Fecha modificación: 2025-03-27
+*/
+
+// Importaciones
 import { VistaLavanderiaDesdeEntrada_2_640 as defaultImage } from 'assets';
 import video from 'assets/video/video1.mp4';
 import { ButtonContainer, ButtonSecondary, Container } from 'components';
 import Video from 'components/video/Video';
 import PropTypes from 'prop-types';
 
+// Estilos
+const HEROINICIO_UI = {
+  HEIGHT:
+    "h-[82vh] sm:h-[500px] xl:h-[70vh]",
+  THEME: "dark",
+  BACKGROUND: {
+    OPACITY: "opacity-90",
+    COLOR: "bg-primary",
+  },
+  DISPLAY: {
+    BUTTON: "block",
+    IMAGE: "block",
+    PARAGRAPH: "hidden",
+  },
+  TITLE: {
+    FONT_SIZE: "text-2xl md:text-4xl",
+    FONT_WEIGHT: "font-bold",
+    FONT_FAMILY: "",
+    LETTER_SPACING: "tracking-wider",
+    LINE_HEIGHT: "",
+    TEXT_TRANSFORM: "",
+  },
+  COLOR: {
+    BUTTON: "primary",
+    PARAGRAPH: "text-light",
+    TITLE: "text-light",
+  },
+  BUTTON: {
+    SIZE: "lg",
+    HREF: "/#servicios",
+    TEXT: {
+      CONTENT: "Nuestros Servicios",
+      COLOR: "light",
+      SIZE: "xl",
+      TRANSFORM: "uppercase",
+    },
+  },
+};
+
+const styles = {
+  button: `bg-${HEROINICIO_UI.COLOR.BUTTON} px-6 py-4 w-[${HEROINICIO_UI.BUTTON.SIZE}] text-${HEROINICIO_UI.BUTTON.TEXT.SIZE} rounded-lg ${HEROINICIO_UI.DISPLAY.BUTTON}  text-${HEROINICIO_UI.BUTTON.TEXT.SIZE} text-${HEROINICIO_UI.BUTTON.TEXT.COLOR}`,
+  paragraph: `text-${HEROINICIO_UI.COLOR.PARAGRAPH} text-lg ${HEROINICIO_UI.DISPLAY.PARAGRAPH}`,
+  buttonContainer: 'place-content-center sm:place-content-start',
+  title: `${HEROINICIO_UI.TITLE.FONT_SIZE} ${HEROINICIO_UI.TITLE.FONT_WEIGHT} ${HEROINICIO_UI.TITLE.FONT_FAMILY} ${HEROINICIO_UI.TITLE.LETTER_SPACING}  ${HEROINICIO_UI.TITLE.TEXT_TRANSFORM} ${HEROINICIO_UI.TITLE.LINE_HEIGHT} ${HEROINICIO_UI.COLOR.TITLE}`,
+  image: `absolute top-0 left-0 overflow-hidden object-cover object-center w-full`
+}
+
+// Estructura
 const HeroInicioVideo = () => {
 
-  const HEROINICIO_UI = {
-    HEIGHT:
-      "h-[82vh] sm:h-[500px] xl:h-[70vh]",
-    THEME: "dark",
-    BACKGROUND: {
-      OPACITY: "opacity-90",
-      COLOR: "bg-primary",
-    },
-    DISPLAY: {
-      BUTTON: "block",
-      IMAGE: "block",
-      PARAGRAPH: "hidden",
-    },
-    TITLE: {
-      FONT_SIZE: "text-2xl md:text-4xl",
-      FONT_WEIGHT: "font-bold",
-      FONT_FAMILY: "",
-      LETTER_SPACING: "tracking-wider",
-      LINE_HEIGHT: "",
-      TEXT_TRANSFORM: "",
-    },
-    COLOR: {
-      BUTTON: "primary",
-      PARAGRAPH: "text-light",
-      TITLE: "text-light",
-    },
-    BUTTON: {
-      SIZE: "lg",
-      HREF: "/#servicios",
-      TEXT: {
-        CONTENT: "Nuestros Servicios",
-        COLOR: "light",
-        SIZE: "xl",
-        TRANSFORM: "uppercase",
-      },
-    },
-  };
-
-  const styles = {
-    button: `bg-${HEROINICIO_UI.COLOR.BUTTON} px-6 py-4 w-[${HEROINICIO_UI.BUTTON.SIZE}] text-${HEROINICIO_UI.BUTTON.TEXT.SIZE} rounded-lg ${HEROINICIO_UI.DISPLAY.BUTTON}  text-${HEROINICIO_UI.BUTTON.TEXT.SIZE} text-${HEROINICIO_UI.BUTTON.TEXT.COLOR}`,
-    paragraph: `text-${HEROINICIO_UI.COLOR.PARAGRAPH} text-lg ${HEROINICIO_UI.DISPLAY.PARAGRAPH}`,
-    buttonContainer: 'place-content-center sm:place-content-start',
-    title: `${HEROINICIO_UI.TITLE.FONT_SIZE} ${HEROINICIO_UI.TITLE.FONT_WEIGHT} ${HEROINICIO_UI.TITLE.FONT_FAMILY} ${HEROINICIO_UI.TITLE.LETTER_SPACING}  ${HEROINICIO_UI.TITLE.TEXT_TRANSFORM} ${HEROINICIO_UI.TITLE.LINE_HEIGHT} ${HEROINICIO_UI.COLOR.TITLE}`,
-    image: `absolute top-0 left-0 overflow-hidden object-cover object-center w-full`
-  }
   return (
     <section id='inicio' className={`relative overflow-hidden w-full ${HEROINICIO_UI.HEIGHT}`}>
       <div className={`absolute inset-0 z-0 w-full bg-dark left-0 ${HEROINICIO_UI.HEIGHT}`}>
