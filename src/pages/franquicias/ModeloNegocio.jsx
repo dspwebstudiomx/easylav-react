a1407ac289ce20740eaf48010573dad0a743d4c8
dspwebstudiomx <dspwebstudio@gmail.com>
2025-03-31 15:58:50 -0600

    Adds new icons and enhances layout for ModeloNegocio component to improve visual appeal and user experience

--- src/pages/franquicias/ModeloNegocio.jsx
+++ src/pages/franquicias/ModeloNegocio.jsx
@@ -10,8 +10,10 @@
 
 // Importaciones
 import { Container, Paragraph, Section, TitleH3, TitleContainer, Spacing } from 'components';
-import { FaCheck } from 'react-icons/fa6';
+import { FaCheck, FaComputer } from 'react-icons/fa6';
 import { MdAttachMoney } from 'react-icons/md';
+import { GiReceiveMoney } from 'react-icons/gi';
+import { GrMoney } from 'react-icons/gr';
 
 // Base de Datos
 const tarjetasModeloNegocio = [
@@ -32,6 +34,7 @@ const tarjetasModeloNegocio = [
     id: 2,
     titulo: 'Costos Operativos',
     descripcion: 'Los costos operativos mensuales incluyen alquiler, servicios, personal, marketing y otros gastos.',
+    icon: <GrMoney size={52} className="text-primary mx-auto" />,
     caracteristicas: ['Alquiler: $BB,BBB', 'Servicios: $CC,CCC', 'Personal: $DD,DDD', 'Marketing: $EE,EEE'],
   },
   {
@@ -39,12 +42,14 @@ const tarjetasModeloNegocio = [
     titulo: 'Márgenes de Ganancia',
     descripcion:
       'Easylav ofrece márgenes de ganancia atractivos gracias a su modelo de negocio eficiente y alta demanda de servicios.',
+    icon: <GiReceiveMoney size={52} className="text-primary mx-auto" />,
     caracteristicas: ['Margen bruto promedio: XX%', 'Retorno de inversión estimado: XX años'],
   },
   {
     id: 4,
     titulo: 'Soporte de Easylav',
     descripcion: 'Easylav apoya a sus franquiciados con capacitación, marketing, tecnología y gestión.',
+    icon: <FaComputer size={52} className="text-primary mx-auto" />,
     caracteristicas: [
       'Capacitación inicial y continua',
       'Soporte de marketing y publicidad',
@@ -64,17 +69,20 @@ const ModeloNegocio = () => {
           El modelo de franquicia de Easylav está diseñado para ofrecer una alta rentabilidad y sostenibilidad. Aquí te
           explicamos cómo funciona:
         </Paragraph>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-20">
           {tarjetasModeloNegocio.map((tarjeta) => (
-            <article key={tarjeta.id} className="shadow-2xl p-8 rounded-2xl bg-light">
+            <article
+              key={tarjeta.id}
+              className="shadow-2xl p-8 rounded-2xl bg-light transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300"
+            >
               {tarjeta.icon}
               <Spacing distance="h-4" />
-              <TitleH3 textTransform="base" fontSize="text-2xl" justify="justify-center">
+              <TitleH3 textTransform="base" fontSize="text-2xl" justify="justify-center" color="text-secondary_dark">
                 {tarjeta.titulo}
               </TitleH3>
               <Spacing distance="h-12" />
               <p className="text-gray-700 text-dark">{tarjeta.descripcion}</p>
-              <ul className="flex flex-col gap-2 bg-light text-dark list-none border-2 border-primary rounded-2xl p-6 mt-10">
+              <ul className="flex flex-col gap-2 text-dark list-none border-4 border-primary rounded-2xl p-6 mt-10 h-auto">
                 {tarjeta.caracteristicas.map((caracteristica, index) => (
                   <li key={`${tarjeta.id}-${index}`} className="flex gap-3 text-base items-center">
                     <FaCheck className="text-secondary_dark" />
