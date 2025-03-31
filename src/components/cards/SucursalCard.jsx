e28378f459249af157060a777e1302a2b5e32c78
dspwebstudiomx <dspwebstudio@gmail.com>
2025-03-31 13:27:59 -0600

    Refactors Prettier configuration and updates ModeloNegocio component for improved layout and accessibility, including icon integration and enhanced text presentation.

--- src/components/cards/SucursalCard.jsx
+++ src/components/cards/SucursalCard.jsx
@@ -12,7 +12,7 @@
 // Importaciones
 import classNames from 'classnames';
 import { BackgroundImageSection, Badge, Modal, Spacing, TitleH3 } from 'components';
-import { useShowModal } from "hooks";
+import { useShowModal } from 'hooks';
 import PropTypes from 'prop-types';
 import { useMemo } from 'react';
 import { FaMapMarkedAlt, FaMapMarkerAlt } from 'react-icons/fa';
@@ -21,43 +21,43 @@ import { FaMagnifyingGlass, FaRegClock, FaWaze, FaXmark } from 'react-icons/fa6'
 // Estilos
 const SUCURSAL_CARD_UI = {
   ARTICLE: {
-    ANIMATION: "animate__animated animate__fadeIn animate__slower ",
-    BACKGROUND: "bg-light",
-    COLOR: "text-dark",
-    DISPLAY: "flex flex-col relative mx-auto",
-    HEIGHT: "h-full",
-    JUSTIFY: "justify-center items-center mx-auto",
-    OVERFLOW: "overflow-hidden",
-    ROUNDED: "rounded-xl",
-    SHADOW: "shadow-xl",
-    WIDTH: "w-[80%] md:w-[280px] lg:w-[220px]",
+    ANIMATION: 'animate__animated animate__fadeIn animate__slower ',
+    BACKGROUND: 'bg-light',
+    COLOR: 'text-dark',
+    DISPLAY: 'flex flex-col relative mx-auto',
+    HEIGHT: 'h-full',
+    JUSTIFY: 'justify-center items-center mx-auto',
+    OVERFLOW: 'overflow-hidden',
+    ROUNDED: 'rounded-xl',
+    SHADOW: 'shadow-xl',
+    WIDTH: 'w-[80%] md:w-[280px] lg:w-[220px]',
   },
   ICON: {
     SIZE_1: 30,
     SIZE_2: 20,
-    COLOR: 'text-secondary'
+    COLOR: 'text-secondary',
   },
   TEXT: {
-    FONT_SIZE: "text-[15px] sm:text-[12px]",
-    COLOR: "text-dark",
-    LINE_HEIGHT: "",
+    FONT_SIZE: 'text-[15px] sm:text-[12px]',
+    COLOR: 'text-dark',
+    LINE_HEIGHT: '',
   },
   CONTENT: {
-    SECTION: "flex flex-col justify-start gap-3 p-4 px-10",
-    TITLE: "flex items-center justify-center mx-auto",
+    SECTION: 'flex flex-col justify-start gap-3 p-4 px-10',
+    TITLE: 'flex items-center justify-center mx-auto',
     DESCRIPTION: {
-      CONTAINER: "flex flex-col gap-4 my-4 justify-center",
-      INFO: "flex gap-6 justify-center"
+      CONTAINER: 'flex flex-col gap-4 my-4 justify-center',
+      INFO: 'flex gap-6 justify-center',
     },
     BADGE: {
-      CONTAINER: 'absolute -left-14 top-0 w-[280px] rotate-[320deg] rounded-lg border-2 border-secondary bg-secondary_light px-4 py-1 text-light flex items-center text-sm',
-      PARAGRAPH: 'ml-8'
-    }
-  }
-}
+      CONTAINER:
+        'absolute -left-14 top-0 w-[280px] rotate-[320deg] rounded-lg border-2 border-secondary bg-secondary_light px-4 py-1 text-light flex items-center text-sm',
+      PARAGRAPH: 'ml-8',
+    },
+  },
+};
 
-const styles =
-{
+const styles = {
   article: `${SUCURSAL_CARD_UI.ARTICLE.ANIMATION} ${SUCURSAL_CARD_UI.ARTICLE.BACKGROUND} ${SUCURSAL_CARD_UI.ARTICLE.COLOR} ${SUCURSAL_CARD_UI.ARTICLE.DISPLAY} ${SUCURSAL_CARD_UI.ARTICLE.HEIGHT} ${SUCURSAL_CARD_UI.ARTICLE.JUSTIFY} ${SUCURSAL_CARD_UI.ARTICLE.OVERFLOW} ${SUCURSAL_CARD_UI.ARTICLE.ROUNDED} ${SUCURSAL_CARD_UI.ARTICLE.SHADOW} ${SUCURSAL_CARD_UI.ARTICLE.WIDTH}`,
   content: {
     section: SUCURSAL_CARD_UI.CONTENT.SECTION,
@@ -65,20 +65,19 @@ const styles =
     icon: {
       size_1: SUCURSAL_CARD_UI.ICON.SIZE_1,
       size_2: SUCURSAL_CARD_UI.ICON.SIZE_2,
-      color: SUCURSAL_CARD_UI.ICON.COLOR
+      color: SUCURSAL_CARD_UI.ICON.COLOR,
     },
     text: `${SUCURSAL_CARD_UI.TEXT.FONT_SIZE} ${SUCURSAL_CARD_UI.TEXT.COLOR} ${SUCURSAL_CARD_UI.TEXT.LINE_HEIGHT}`,
     description: {
       container: SUCURSAL_CARD_UI.CONTENT.DESCRIPTION.CONTAINER,
-      info: SUCURSAL_CARD_UI.CONTENT.DESCRIPTION.INFO
+      info: SUCURSAL_CARD_UI.CONTENT.DESCRIPTION.INFO,
     },
     badge: {
       container: SUCURSAL_CARD_UI.CONTENT.BADGE.CONTAINER,
       paragraph: SUCURSAL_CARD_UI.CONTENT.BADGE.PARAGRAPH,
-    }
+    },
   },
-
-}
+};
 
 // Funciones
 
@@ -93,22 +92,36 @@ const isBranchCurrentlyOpen = (openHour, openMinute, closeHour, closeMinute) => 
 
 // Function to render the open/closed badge
 const renderOpenClosedBadge = (isOpen) => (
-  <div className={`rounded-lg border-2 text-xs ${isOpen ? ' border-primary_darkcontrast bg-primary text-primary_darkcontrast' : 'border-red_dark bg-red_light text-red'} px-4 py-2`}>
+  <div
+    className={`rounded-lg border-2 text-xs ${isOpen ? ' border-primary_darkcontrast bg-primary text-primary_darkcontrast' : 'border-red_dark bg-red_light text-red'} px-4 py-2`}
+  >
     <p>{isOpen ? 'Abierto' : 'Cerrado'}</p>
   </div>
 );
 
 // Function to render Waze and Google Maps links
 const renderMapLinks = (position, title, gmap) => (
-  <div className='flex md:hidden'>
-    <a href={`https://waze.com/ul?ll=${position.lat},${position.lng}&navigate=yes`} target='_blank' rel='noopener noreferrer' title={`Ir a ${title} en Waze`} className='flex w-full flex-col items-center justify-center gap-2 bg-secondary text-sm'>
-      <Badge flexDirection='flex-col'>
+  <div className="flex md:hidden">
+    <a
+      href={`https://waze.com/ul?ll=${position.lat},${position.lng}&navigate=yes`}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={`Ir a ${title} en Waze`}
+      className="flex w-full flex-col items-center justify-center gap-2 bg-secondary text-sm"
+    >
+      <Badge flexDirection="flex-col">
         <FaWaze size={24} />
         <span>Waze</span>
       </Badge>
     </a>
-    <a href={gmap} target='_blank' rel='noopener noreferrer' title={`Ir a ${title} en Google Maps`} className='flex w-full flex-col items-center justify-center gap-2 bg-secondary_dark text-sm'>
-      <Badge flexDirection='flex-col'>
+    <a
+      href={gmap}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={`Ir a ${title} en Google Maps`}
+      className="flex w-full flex-col items-center justify-center gap-2 bg-secondary_dark text-sm"
+    >
+      <Badge flexDirection="flex-col">
         <FaMapMarkedAlt size={24} />
         <span>Google Maps</span>
       </Badge>
@@ -118,14 +131,29 @@ const renderMapLinks = (position, title, gmap) => (
 
 // Estructura
 const SucursalCard = (props) => {
-
-  let { title, position, place, gmap, badge, advertisement, image, serviceday1, servicehour1, openHour, closeHour, openMinute, closeMinute } = props;
-  const isOpen = useMemo(() => isBranchCurrentlyOpen(openHour, openMinute, closeHour, closeMinute), [openHour, openMinute, closeHour, closeMinute]);
-  const { showModal, setShowModal } = useShowModal()
+  let {
+    title,
+    position,
+    place,
+    gmap,
+    badge,
+    advertisement,
+    image,
+    serviceday1,
+    servicehour1,
+    openHour,
+    closeHour,
+    openMinute,
+    closeMinute,
+  } = props;
+  const isOpen = useMemo(
+    () => isBranchCurrentlyOpen(openHour, openMinute, closeHour, closeMinute),
+    [openHour, openMinute, closeHour, closeMinute]
+  );
+  const { showModal, setShowModal } = useShowModal();
 
   return (
     <article id={`sucursal-${title}`} key={title} className={classNames(styles.article, { 'z-10': showModal })}>
-
       {/* Imagen */}
       <BackgroundImageSection
         id={`sucursal-${title}`}
@@ -136,16 +164,24 @@ const SucursalCard = (props) => {
         image_1024={image}
         image_1200={image}
         image_1920={image}
-        height='h-[160px]'
-        opacity='opacity-30'
-        backgroundColor='bg-dark'
-        className='p-4 rounded-t-xl'>
-        <div id={`open-closed-zoom-${title}`} className='flex justify-end items-start h-full absolute top-0 right-0 p-4 w-full'>
+        height="h-[160px]"
+        opacity="opacity-30"
+        backgroundColor="bg-dark"
+        className="p-4 rounded-t-xl"
+      >
+        <div
+          id={`open-closed-zoom-${title}`}
+          className="flex justify-end items-start h-full absolute top-0 right-0 p-4 w-full"
+        >
           {/* Badge Horario Abierto/Cerrado */}
           {renderOpenClosedBadge(isOpen)}
         </div>
-        <div className='flex justify-end items-end h-full absolute top-0 right-0 p-4 w-full'>
-          <button onClick={() => setShowModal(true)} id='zoom' className='text-light rounded-full border-2 border-light p-3 opacity-40 bg-dark hover:bg-secondary hover:opacity-100 cursor-pointer'>
+        <div className="flex justify-end items-end h-full absolute top-0 right-0 p-4 w-full">
+          <button
+            onClick={() => setShowModal(true)}
+            id="zoom"
+            className="text-light rounded-full border-2 border-light p-3 opacity-40 bg-dark hover:bg-secondary hover:opacity-100 cursor-pointer"
+          >
             <FaMagnifyingGlass />
           </button>
         </div>
@@ -153,50 +189,52 @@ const SucursalCard = (props) => {
       {/* Imagen */}
 
       {/* Descripción */}
-      <section className='flex flex-col gap-4 justify-between items-center min-h-[240px] '>
-        <div className='p-8 flex flex-col gap-4'>
+      <section className="flex flex-col gap-4 justify-between items-center min-h-[240px] ">
+        <div className="p-8 flex flex-col gap-4">
           {/* Título */}
-          <TitleH3 justify='justify-center' color='text-dark' fontSize='text-base' textTransform='uppercase'>
+          <TitleH3 justify="justify-center" color="text-dark" fontSize="text-base" textTransform="uppercase">
             {title}
           </TitleH3>
           {/* Título */}
 
-          <div className='flex flex-col gap-4 items-start'>
-          {/* Dirección */}
-          <a className={styles.content.description.info} href={gmap} target='_blank' rel='noopener noreferrer' title={`Sucursal ${title}`}>
-            <FaMapMarkerAlt size={styles.content.icon.size_1} className={styles.content.icon.color} />
-            <span className={styles.content.text}>{place}</span>
-          </a>
-          {/* Dirección */}
+          <div className="flex flex-col gap-4 items-start">
+            {/* Dirección */}
+            <a
+              className={styles.content.description.info}
+              href={gmap}
+              target="_blank"
+              rel="noopener noreferrer"
+              title={`Sucursal ${title}`}
+            >
+              <FaMapMarkerAlt size={styles.content.icon.size_1} className={styles.content.icon.color} />
+              <span className={styles.content.text}>{place}</span>
+            </a>
+            {/* Dirección */}
 
-          {/* Horario */}
-          <div className={styles.content.description.info}>
-            <FaRegClock size={styles.content.icon.size_2} className={styles.content.icon.color} />
-            <div className={`${styles.content.text} flex flex-col gap-1`}>
-              <span>{serviceday1}</span>
-              <span>{servicehour1}</span>
+            {/* Horario */}
+            <div className={styles.content.description.info}>
+              <FaRegClock size={styles.content.icon.size_2} className={styles.content.icon.color} />
+              <div className={`${styles.content.text} flex flex-col gap-1`}>
+                <span>{serviceday1}</span>
+                <span>{servicehour1}</span>
+              </div>
             </div>
+            {/* Horario */}
           </div>
-          {/* Horario */}
-          </div>
-
         </div>
-
       </section>
-        {/* Advertisement */}
-        {advertisement ? (
-          <section className='w-full'>
-            <Badge backgroundColor='bg-gradient-to-r from-secondary_dark to-secondary_light md:bg-secondary'>
-              <p className='text-sm uppercase px-8'>{advertisement}</p>
-            </Badge>
-            {renderMapLinks(position, title, gmap)}
-          </section>
-        ) : (
-          <div className='w-full'>
-            {renderMapLinks(position, title, gmap)}
-          </div>
-        )}
-        {/* Advertisement */}
+      {/* Advertisement */}
+      {advertisement ? (
+        <section className="w-full">
+          <Badge backgroundColor="bg-gradient-to-r from-secondary_dark to-secondary_light md:bg-secondary">
+            <p className="text-sm uppercase px-8">{advertisement}</p>
+          </Badge>
+          {renderMapLinks(position, title, gmap)}
+        </section>
+      ) : (
+        <div className="w-full">{renderMapLinks(position, title, gmap)}</div>
+      )}
+      {/* Advertisement */}
       {/* Descripción */}
 
       {/* Badge*/}
@@ -209,15 +247,12 @@ const SucursalCard = (props) => {
 
       {/* Modal */}
       {showModal && (
-        <Modal width='w-[80vw] md:w-[60vw] lg:w-auto z-50'>
-          <div id='imagen-sucursal' className="mx-auto flex flex-col rounded-xl border-4 border-primary bg-light p-8">
+        <Modal width="w-[80vw] md:w-[60vw] lg:w-auto z-50">
+          <div id="imagen-sucursal" className="mx-auto flex flex-col rounded-xl border-4 border-primary bg-light p-8">
             <button id="button-close" onClick={() => setShowModal(false)}>
-              <FaXmark
-                size={36}
-                className="z-30 ml-auto text-primary_dark"
-              />
+              <FaXmark size={36} className="z-30 ml-auto text-primary_dark" />
             </button>
-            <Spacing distance='my-4' />
+            <Spacing distance="my-4" />
             <div className="flex flex-col items-center justify-center gap-8">
               <img src={image} alt={title} className="w-[340px] h-[280px] object-cover" />
               <TitleH3>{title}</TitleH3>
@@ -228,7 +263,7 @@ const SucursalCard = (props) => {
       {/* Modal */}
     </article>
   );
-}
+};
 
 SucursalCard.propTypes = {
   title: PropTypes.string,
--- src/pages/franquicias/ModeloNegocio.jsx
+++ src/pages/franquicias/ModeloNegocio.jsx
@@ -1,78 +1,82 @@
-import { Container, Paragraph, Section, TitleH3, TitleContainer, Spacing } from "components";
-import { FaCheck } from "react-icons/fa6";
+import { Container, Paragraph, Section, TitleH3, TitleContainer, Spacing } from 'components';
+import { FaCheck } from 'react-icons/fa6';
+import { MdAttachMoney } from 'react-icons/md';
 
-
-const tarjetasModeloNegocio = [ 
+const tarjetasModeloNegocio = [
   {
     id: 1,
-    titulo:'Inversión Inicial',
-    descripcion:'La inversión inicial incluye el canon de entrada, adecuación del local, equipamiento y stock inicial.',
+    titulo: 'Inversión Inicial',
+    descripcion:
+      'La inversión inicial incluye el canon de entrada, adecuación del local, equipamiento y stock inicial.',
     caracteristicas: [
       'Canon de entrada: $XX,XXX',
       'Adecuación del local: $YY,YYY',
       'Equipamiento: $ZZ,ZZZ',
-      'Stock inicial: $AA,AAA'
-    ]
+      'Stock inicial: $AA,AAA',
+    ],
+    icon: <MdAttachMoney />,
+    iconSize: 32,
   },
   {
     id: 2,
-    titulo:'Costos Operativos',
-    descripcion:'Los costos operativos mensuales incluyen alquiler, servicios, personal, marketing y otros gastos.',
-    caracteristicas: [
-      'Alquiler: $BB,BBB',
-      'Servicios: $CC,CCC',
-      'Personal: $DD,DDD',
-      'Marketing: $EE,EEE'
-    ]
+    titulo: 'Costos Operativos',
+    descripcion: 'Los costos operativos mensuales incluyen alquiler, servicios, personal, marketing y otros gastos.',
+    caracteristicas: ['Alquiler: $BB,BBB', 'Servicios: $CC,CCC', 'Personal: $DD,DDD', 'Marketing: $EE,EEE'],
   },
   {
     id: 3,
-    titulo:'Márgenes de Ganancia',
-    descripcion:'Easylav ofrece márgenes de ganancia atractivos gracias a su modelo de negocio eficiente y alta demanda de servicios.',
-    caracteristicas: [
-      'Margen bruto promedio: XX%',
-      'Retorno de inversión estimado: XX años'
-    ]
+    titulo: 'Márgenes de Ganancia',
+    descripcion:
+      'Easylav ofrece márgenes de ganancia atractivos gracias a su modelo de negocio eficiente y alta demanda de servicios.',
+    caracteristicas: ['Margen bruto promedio: XX%', 'Retorno de inversión estimado: XX años'],
   },
   {
     id: 4,
-    titulo:'Soporte de Easylav',
-    descripcion:'Easylav apoya a sus franquiciados con capacitación, marketing, tecnología y gestión.',
+    titulo: 'Soporte de Easylav',
+    descripcion: 'Easylav apoya a sus franquiciados con capacitación, marketing, tecnología y gestión.',
     caracteristicas: [
       'Capacitación inicial y continua',
       'Soporte de marketing y publicidad',
       'Plataforma tecnológica avanzada',
-      'Asesoramiento en gestión y operaciones'
-    ]
-  }
-]
+      'Asesoramiento en gestión y operaciones',
+    ],
+  },
+];
 
 const ModeloNegocio = () => {
   return (
     <Section className="">
       <Container className="flex flex-col gap-12">
-      <TitleContainer title='Modelo de Negocio Detallado'/>
+        <TitleContainer title="Modelo de Negocio Detallado" />
         <Paragraph>
-          El modelo de franquicia de Easylav está diseñado para ofrecer una alta rentabilidad y sostenibilidad. Aquí te explicamos cómo funciona:
+          El modelo de franquicia de Easylav está diseñado para ofrecer una alta rentabilidad y sostenibilidad. Aquí te
+          explicamos cómo funciona:
         </Paragraph>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          {tarjetasModeloNegocio.map((tarjeta)=>
-             <article key={tarjeta.id} className="shadow-2xl p-8 rounded-2xl bg-light">
-              <TitleH3 textTransform='base' fontSize='text-xl'>{tarjeta.titulo}</TitleH3>
-              <Spacing distance='h-4'/>
-              <p className="text-gray-700 text-dark">
-                {tarjeta.descripcion}
-              </p>
+          {tarjetasModeloNegocio.map((tarjeta) => (
+            <article key={tarjeta.id} className="shadow-2xl p-8 rounded-2xl bg-light">
+              <img
+                className={'text-secondary'}
+                src="tarjeta.icon"
+                title={tarjeta.title}
+                alt={tarjeta.title}
+                size={tarjeta.iconSize}
+              />
+              <TitleH3 textTransform="base" fontSize="text-2xl" justify="justify-center">
+                {tarjeta.titulo}
+              </TitleH3>
+              <Spacing distance="h-4" />
+              <p className="text-gray-700 text-dark">{tarjeta.descripcion}</p>
               <ul className="flex flex-col gap-2 bg-light text-dark list-none border-2 border-primary rounded-2xl p-6 mt-10">
                 {tarjeta.caracteristicas.map((caracteristica, index) => (
                   <li key={`${tarjeta.id}-${index}`} className="flex gap-3 text-base items-center">
                     <FaCheck className="text-secondary_dark" />
                     <span>{caracteristica}</span>
                   </li>
-                ))}   
+                ))}
               </ul>
-           </article>
-          )}
+            </article>
+          ))}
         </div>
       </Container>
     </Section>
