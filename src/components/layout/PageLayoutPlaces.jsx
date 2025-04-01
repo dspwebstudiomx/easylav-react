5ecb60889e8d94a4331423f4fbc6352218d4ee1d
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-01 10:25:50 -0600

    Refactoriza el componente PageLayoutPlaces para mejorar la legibilidad y la estructura del código.

--- src/components/layout/PageLayoutPlaces.jsx
+++ src/components/layout/PageLayoutPlaces.jsx
@@ -6,22 +6,27 @@
 */
 
 // Importaciones
-import { BackgroundImageSection, ButtonContainer, ButtonSecondary, Contacto, Navbar, ScrollToTopButton, SucursalCardPerCity } from 'components'
-import PropTypes from 'prop-types'
-import Container from '../containers/Container'
-import Footer from '../templates/footer/Footer'
-import Section from '../templates/Section'
-import Spacing from './Spacing'
-
+import {
+  BackgroundImageSection,
+  ButtonContainer,
+  ButtonSecondary,
+  Contacto,
+  Navbar,
+  ScrollToTopButton,
+  SucursalCardPerCity,
+} from 'components';
+import PropTypes from 'prop-types';
+import Container from '../containers/Container';
+import Footer from '../templates/footer/Footer';
+import Section from '../templates/Section';
+import Spacing from './Spacing';
 
 // Estructura
 const PageLayoutPlaces = ({ city, image_576, image_1024, image_1200, image_1920 }) => {
-
   return (
     <>
       <Navbar />
-      <main id={`sucursales-${city}`} className='mt-[100px] sm:mt-[60px] md:mt-0 xl:mt-[100px]'>
-
+      <main id={`sucursales-${city}`} className="mt-[100px] sm:mt-[60px] md:mt-0 xl:mt-[100px]">
         {/* Banner */}
         <BackgroundImageSection
           // image_768={image_768}
@@ -30,40 +35,45 @@ const PageLayoutPlaces = ({ city, image_576, image_1024, image_1200, image_1920 
           image_1024={image_1024}
           image_1200={image_1200}
           image_1920={image_1920}
-          height='h-[210px] sm:h-[50vh] '
-          opacity='opacity-50 hover:opacity-60'
-          backgroundColor='bg-dark'
-          className='flex justify-center items-center'
+          height="h-[210px] sm:h-[50vh] "
+          opacity="opacity-50 hover:opacity-60"
+          backgroundColor="bg-dark"
+          className="flex justify-center items-center"
         >
           <h2 className="text-light uppercase text-3xl font-semibold tracking-wide">{city}</h2>
         </BackgroundImageSection>
         {/* Banner */}
 
         {/* Contenido */}
-        <Section id='sucursales-morelia' className={'flex flex-col items-center justify-center gap-6'} height={'h-full'} backgroundColor={'bg-light'}>
+        <Section
+          id="sucursales-morelia"
+          className={'flex flex-col items-center justify-center gap-6'}
+          height={'h-full'}
+          backgroundColor={'bg-light'}
+        >
           <Container className={''}>
             <SucursalCardPerCity city={city} />
-            <ButtonContainer position='items-center justify-center' distance='mt-16'>
+            <Spacing distance={'mt-[10vh]'} />
+            <ButtonContainer position="items-center justify-center" distance="mt-16">
               <ButtonSecondary
-                title='Regresar a sucursales'
-                href='/sucursales'
-                distance=''
-                type='button'
-                width={"w-full sm:w-[320px]"}
+                title="Regresar a sucursales"
+                href="/sucursales"
+                distance=""
+                type="button"
+                width={'w-full sm:w-[320px]'}
               />
             </ButtonContainer>
             <Spacing distance={'mt-[10vh]'} />
-            <Contacto />
           </Container>
+          <Contacto />
         </Section>
         {/* Contenido */}
-
-      </main >
+      </main>
       <Footer />
       <ScrollToTopButton />
     </>
-  )
-}
+  );
+};
 PageLayoutPlaces.propTypes = {
   children: PropTypes.node,
   city: PropTypes.string,
@@ -71,7 +81,6 @@ PageLayoutPlaces.propTypes = {
   image_1024: PropTypes.string,
   image_1200: PropTypes.string,
   image_1920: PropTypes.string,
+};
 
-}
-
-export default PageLayoutPlaces
+export default PageLayoutPlaces;
