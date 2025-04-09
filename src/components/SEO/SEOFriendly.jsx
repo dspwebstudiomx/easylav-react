b4441927af3e7a377b87dc241d4b5c391eb503ce
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-09 12:31:27 -0600

    fix: agrega soporte para Facebook Pixel, Google Tag Manager y Google Analytics en el componente SEOFriendly

--- src/components/SEO/SEOFriendly.jsx
+++ src/components/SEO/SEOFriendly.jsx
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
+// Acceso a las variables de entorno
+const facebookPixelId = import.meta.env.REACT_APP_FACEBOOK_PIXEL_ID || '';
+const googleAnalyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID || '';
+const googleTagManagerId = import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID || '';
+const googleAdsId = import.meta.env.VITE_GOOGLE_ADS_ID || '';
+
 const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogImageAlt, ogType, publisher }) => {
   return (
     <Helmet>
@@ -15,21 +21,7 @@ const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogIma
       <meta name="robots" content="index, follow" />
       <link rel="canonical" href={url} />
       <meta name="referrer" content="no-referrer-when-downgrade" />
-      {/* SEO adicional */}
-      <meta name="language" content="es" />
-      <meta name="geo.region" content="MX" />
-      <meta name="geo.placename" content="México" />
-      <meta name="copyright" content="© 2025 Easylav. Todos los derechos reservados." />
-      {/* DNS Prefetch */}
-      <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-      <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-      <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
-      <link rel="dns-prefetch" href="//www.google-analytics.com" />
-      {/* Preconnect */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
-      <link rel="preconnect" href="https://www.google-analytics.com" />
+
       {/* Open Graph (OG) para redes sociales */}
       <meta property="og:type" content={ogType} />
       <meta property="og:url" content={url} />
@@ -39,9 +31,10 @@ const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogIma
       <meta property="og:image:alt" content={ogImageAlt} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
-      |<meta property="og:site_name" content="Easylav" />
+      <meta property="og:site_name" content="Easylav" />
       <meta property="og:locale" content="es_MX" />
       <meta property="og:locale:alternate" content="en_US" />
+
       {/* Twitter Cards */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:url" content={url} />
@@ -51,10 +44,7 @@ const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogIma
       <meta name="twitter:site" content="@easylav" />
       <meta name="twitter:creator" content="@easylav" />
       <meta name="twitter:image:alt" content={ogImageAlt} />
-      <meta name="twitter:image:width" content="1200" />
-      <meta name="twitter:image:height" content="630" />
-      <meta name="twitter:label1" content="Precio" />
-      <meta name="twitter:data1" content="Desde $40.00 el kilo" />
+
       {/* Favicon y tema */}
       <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-touch-icon.png" />
@@ -62,63 +52,83 @@ const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogIma
       <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png" />
       <meta name="theme-color" content="#AF4DFA" />
       <link rel="manifest" href="/assets/images/favicon/site.webmanifest" />
-      {/* Seguridad*/}
-      <meta httpEquiv="X-XSS-Protection" content="1; mode=block" />
-      <meta httpEquiv="X-Permitted-Cross-Domain-Policies" content="none" />
-      <meta httpEquiv="X-Download-Options" content="noopen" />
-      <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self';" />
-      <meta httpEquiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains; preload" />
-      <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
-      <meta httpEquiv="X-Frame-Options" content="DENY" />
-      <meta httpEquiv="Referrer-Policy" content="no-referrer" />
-      <meta httpEquiv="Permissions-Policy" content="geolocation=(self), microphone=()" />
-      <meta httpEquiv="Cross-Origin-Embedder-Policy" content="require-corp" />
-      <meta httpEquiv="Cross-Origin-Opener-Policy" content="same-origin" />
-      <meta httpEquiv="Cross-Origin-Resource-Policy" content="same-origin" />
+
+      {/* Facebook Pixel */}
+      <script>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${facebookPixelId}');
+          fbq('track', 'PageView');
+        `}
+      </script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${facebookPixelId}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+
+      {/* Google Tag Manager */}
+      <script>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=${googleTagManagerId}';f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${googleTagManagerId}');
+        `}
+      </script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
+
+      {/* Google Analytics */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${googleAnalyticsId}');
+        `}
+      </script>
+
+      {/* Google Ads Conversion Tracking */}
+      <script>
+        {`
+          gtag('event', 'conversion', {
+            'send_to': '${googleAdsId}'
+          });
+        `}
+      </script>
+
+      {/* Content-Security-Policy */}
       <meta
         httpEquiv="Content-Security-Policy"
         content="
-          default-src 'self'; 
-          script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com; 
-          style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; 
-          font-src 'self' https://fonts.gstatic.com; 
-          img-src 'self' data: https://easylav.mx https://cdn.jsdelivr.net https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://www.gstatic.com; 
-          connect-src 'self' https://api.easylav.mx https://www.google-analytics.com https://www.googletagmanager.com https://www.googleapis.com; 
-          media-src 'self'; 
-          frame-src 'self' https://www.google.com; 
+          default-src 'self';
+          script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com;
+          style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+          font-src 'self' https://fonts.gstatic.com;
+          img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://connect.facebook.net https://www.google.com https://www.gstatic.com https://www.facebook.com https://i.ytimg.com;
+          connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://connect.facebook.net https://www.googleapis.com;
+          frame-src 'self' https://www.google.com https://www.googletagmanager.com https://www.youtube.com;
           object-src 'none';
         "
       />
-      {/* Schema */}
-      <script type="application/ld+json">
-        {JSON.stringify({
-          '@context': 'https://schema.org',
-          '@type': 'LocalBusiness',
-          name: title,
-          description: description,
-          url: url,
-          telephone: '+524435200593',
-          address: {
-            '@type': 'PostalAddress',
-            streetAddress: 'Av Francisco I. Madero Ote 2162, Isaac Arriaga, 58210 Morelia, Mich.',
-            addressLocality: 'Morelia',
-            addressRegion: 'Michoacán',
-            addressCountry: 'México',
-          },
-          image: ogImage,
-          openingHours: 'Mo-Su 07:00-21:30',
-          priceRange: '$$',
-          geo: {
-            '@type': 'GeoCoordinates',
-            latitude: 19.70764,
-            longitude: -101.17192,
-          },
-          sameAs: [
-            'https://www.facebook.com/profile.php?id=61562871132158',
-            'https://www.instagram.com/lavanderiaseasylav/',
-          ],
-        })}
-      </script>
     </Helmet>
   );
 };
