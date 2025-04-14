89b3a925dc6a57365b8073cbdc5266c61a8ba404
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-14 14:08:34 -0600

    refactor: actualiza la documentación y mejora la estructura del componente Navbar para mayor claridad y responsividad

--- src/components/templates/Header/navbar/Navbar.jsx
+++ src/components/templates/Header/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 /*
-  Navbar
+  Navbar.jsx - 
   =====================================
   Creado por : Daniel Pérez
   Fecha: 2024-08-12
+  Descripción: Componente de navegación principal del sitio web. Contiene enlaces de navegación, un botón de modo oscuro y un logotipo. Se adapta a diferentes tamaños de pantalla.
+  Utiliza un estado para manejar la visibilidad de los enlaces de navegación en dispositivos móviles.
+  Dependencias: React, componentes personalizados (Container, DarkModeButton, HeaderLine, Logo, MenuButton, NavLinksDesktop, NavLinksMobile, NavLinksTablet).
+  Estilos: Utiliza Tailwind CSS para el diseño y la responsividad.
+  Responsividad: Se adapta a diferentes tamaños de pantalla utilizando clases de Tailwind CSS. El menú de navegación se oculta en pantallas más pequeñas y se muestra en pantallas más grandes. El botón de modo oscuro es accesible en todas las pantallas.
+  Funcionalidad: Permite a los usuarios navegar por el sitio web y cambiar entre el modo claro y oscuro. El menú de navegación se puede abrir y cerrar en dispositivos móviles.
+  Mejora de Accesibilidad: Se asegura de que los enlaces de navegación sean accesibles y fáciles de usar en todos los dispositivos.
+  Mejora de SEO: Utiliza etiquetas semánticas y una estructura clara para mejorar la indexación en motores de búsqueda.
+  =====================================
+  Modificaciones: 
 */
 
 // Importaciones
@@ -29,7 +39,7 @@ const Navbar = () => {
     toggleNav();
   };
 
-  const height = 'h-[80px] sm:h-[180px] md:h-[100px]';
+  const height = 'h-[80px] sm:h-[180px] md:h-[160px] xl:h-[100px]';
 
   return (
     <>
@@ -49,7 +59,7 @@ const Navbar = () => {
           </div>
         </Container>
       </nav>
-      <HeaderLine className="fixed top-[80px] sm:top-[100px]" />
+      <HeaderLine className="fixed sm:relative top-[80px] sm:top-[10px] xl:top-[100px]" />
     </>
   );
 };
