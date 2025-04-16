82d6cb4c893248792780478ee9c8c0afda8893dd
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-16 13:26:00 -0600

    refactor: reorganiza la estructura del componente QuienesSomos para mejorar la presentación y la legibilidad

--- src/pages/inicio/components/QuienesSomos.jsx
+++ src/pages/inicio/components/QuienesSomos.jsx
@@ -129,112 +129,114 @@ const QuienesSomos = () => {
       <Container className="mx-auto flex-col gap-20" id="quienes-somos-contenedor">
         <TitleContainer id="quienes-somos-titulo" title="Quienes somos" />
 
-        {/* Párrafos */}
-        <ParagraphContainer>
-          <Paragraph>
-            Somos una empresa de lavanderías y Tintorería, comprometida en darte las mejores soluciones para la limpieza
-            y cuidado de tu ropa.
-          </Paragraph>
-          <Paragraph>
-            Comprometidos en brindarte el mejor servicio en cuanto al lavado de tus prendas, contamos con un servicio
-            personalizado en nuestras sucursales con modernas instalaciones e imagen fresca, así como servicios que se
-            adaptan a tus necesidades, sea lavado y secado por encargo o autoservicio y el cuidado de tu ropa de
-            tintorería. Al entender tu dinámica cotidiana, uno de nuestros objetivos fundamentales es{' '}
-            <span className="text-secondary_dark dark:text-secondary_light font-bold">ahorrarte tiempo</span> y
-            contribuir a tu calidad de vida.
-          </Paragraph>
-          <Paragraph>
-            Nuestra Visión de negocio nos exige seguir desarrollando e implementando nuevos y mejores procedimientos con
-            tecnología de punta, con la cual ofrecemos costos bajos de operación brindándote el servicio que necesitas y
-            contribuyendo al planeta en el cuidado del agua y optimización de recursos.{' '}
-          </Paragraph>
-        </ParagraphContainer>
-        {/* Párrafos */}
+        <div className="flex flex-col gap-12 items-center justify-center mx-auto w-full h-full">
+          {/* Párrafos */}
+          <ParagraphContainer>
+            <Paragraph>
+              Somos una empresa de lavanderías y Tintorería, comprometida en darte las mejores soluciones para la
+              limpieza y cuidado de tu ropa.
+            </Paragraph>
+            <Paragraph>
+              Comprometidos en brindarte el mejor servicio en cuanto al lavado de tus prendas, contamos con un servicio
+              personalizado en nuestras sucursales con modernas instalaciones e imagen fresca, así como servicios que se
+              adaptan a tus necesidades, sea lavado y secado por encargo o autoservicio y el cuidado de tu ropa de
+              tintorería. Al entender tu dinámica cotidiana, uno de nuestros objetivos fundamentales es{' '}
+              <span className="text-secondary_dark dark:text-secondary_light font-bold">ahorrarte tiempo</span> y
+              contribuir a tu calidad de vida.
+            </Paragraph>
+            <Paragraph>
+              Nuestra Visión de negocio nos exige seguir desarrollando e implementando nuevos y mejores procedimientos
+              con tecnología de punta, con la cual ofrecemos costos bajos de operación brindándote el servicio que
+              necesitas y contribuyendo al planeta en el cuidado del agua y optimización de recursos.{' '}
+            </Paragraph>
+          </ParagraphContainer>
+          {/* Párrafos */}
 
-        {/* Misión & Visión */}
-        <TwoColumnsContainer>
-          {/* Misión */}
-          <Card>
-            <BorderLeft>
-              <h3 className="text-2xl font-semibold uppercase text-dark">Misión</h3>
-            </BorderLeft>
-            <p className="text-lg text-dark">
-              En Lavanderías <span className="text-lg font-bold text-secondary_dark">EASYLAV</span>, nuestra misión es
-              ser los aliados perfectos de tu tiempo, ofreciéndote un servicio de lavandería y tintorería de excelencia.
-              Nos dedicamos a cuidar cada prenda como si fuera nuestra, utilizando tecnología de vanguardia y procesos
-              especializados para garantizar resultados impecables. Con sucursales diseñadas para tu comodidad y
-              atención personalizada, transformamos el cuidado de tu ropa en una experiencia práctica y confiable, para
-              que puedas dedicar tu tiempo a lo que realmente importa.
-            </p>
-          </Card>
-          {/* Misión */}
-
-          {/* Visión */}
-          <Card>
-            <BorderLeft>
-              <h3 className="text-2xl font-semibold uppercase text-dark">Visión</h3>
-            </BorderLeft>
-            <p className="text-lg text-dark">
-              Consolidar una red de franquicias de lavanderías a nivel nacional, respaldada por un modelo operativo
-              eficiente, capacitación continua y un enfoque en la mejora constante de procesos. Nos distinguimos por la
-              innovación tecnológica, la excelencia en el servicio y la optimización de costos, garantizando un negocio
-              escalable, confiable y altamente rentable para nuestros franquiciatarios.
-            </p>
-          </Card>
-          {/* Visión */}
-        </TwoColumnsContainer>
-        {/* Misión & Visión */}
-
-        {/* Nuestros Valores */}
-        <Card>
-          <BorderLeft>
-            <h3 className="text-2xl font-semibold uppercase text-dark">Nuestros Valores</h3>
-          </BorderLeft>
-          <NuestrosValoresGeneradorTarjetas />
-        </Card>
-        {/* Nuestros Valores */}
-
-        {/* Servicios */}
-        <Card>
+          {/* Misión & Visión */}
           <TwoColumnsContainer>
-            <div className="flex flex-col gap-12 items-start justify-center text-dark">
+            {/* Misión */}
+            <Card>
               <BorderLeft>
-                <h3 className="text-2xl font-semibold uppercase">Servicios</h3>
+                <h3 className="text-2xl font-semibold uppercase text-dark">Misión</h3>
               </BorderLeft>
-              <div className="grid gap-3">
-                {serviciosOfrecidos.map((servicio) => {
-                  return (
-                    <article
-                      key={servicio.id}
-                      id={`servicio-${servicio.id}`}
-                      className="flex gap-4 items-center justify-start">
-                      <div className="w-8 h-8">
-                        <FaCheck className="text-secondary" size={28} />
-                      </div>
-                      <span className="text-lg">{servicio.title}</span>
-                    </article>
-                  );
-                })}
-              </div>
-              <ButtonContainer position="items-center justify-start" distance="h-20">
-                <Button
-                  title="Contáctanos"
-                  href="/contacto"
-                  width="w-full sm:w-[260px] scale-[0.8]"
-                  variant="secondary"
-                />
-              </ButtonContainer>
-            </div>
-            <img
-              src={image}
-              alt="vista interna de lavandería Easylav con persona"
-              title="lavandería Easylav"
-              className="shadow-image"
-              loading="lazy"
-            />
+              <p className="text-lg text-dark">
+                En Lavanderías <span className="text-lg font-bold text-secondary_dark">EASYLAV</span>, nuestra misión es
+                ser los aliados perfectos de tu tiempo, ofreciéndote un servicio de lavandería y tintorería de
+                excelencia. Nos dedicamos a cuidar cada prenda como si fuera nuestra, utilizando tecnología de
+                vanguardia y procesos especializados para garantizar resultados impecables. Con sucursales diseñadas
+                para tu comodidad y atención personalizada, transformamos el cuidado de tu ropa en una experiencia
+                práctica y confiable, para que puedas dedicar tu tiempo a lo que realmente importa.
+              </p>
+            </Card>
+            {/* Misión */}
+
+            {/* Visión */}
+            <Card>
+              <BorderLeft>
+                <h3 className="text-2xl font-semibold uppercase text-dark">Visión</h3>
+              </BorderLeft>
+              <p className="text-lg text-dark">
+                Consolidar una red de franquicias de lavanderías a nivel nacional, respaldada por un modelo operativo
+                eficiente, capacitación continua y un enfoque en la mejora constante de procesos. Nos distinguimos por
+                la innovación tecnológica, la excelencia en el servicio y la optimización de costos, garantizando un
+                negocio escalable, confiable y altamente rentable para nuestros franquiciatarios.
+              </p>
+            </Card>
+            {/* Visión */}
           </TwoColumnsContainer>
-        </Card>
-        {/* Servicios */}
+          {/* Misión & Visión */}
+
+          {/* Nuestros Valores */}
+          <Card>
+            <BorderLeft>
+              <h3 className="text-2xl font-semibold uppercase text-dark">Nuestros Valores</h3>
+            </BorderLeft>
+            <NuestrosValoresGeneradorTarjetas />
+          </Card>
+          {/* Nuestros Valores */}
+
+          {/* Servicios */}
+          <Card>
+            <TwoColumnsContainer>
+              <div className="flex flex-col gap-12 items-start justify-center text-dark">
+                <BorderLeft>
+                  <h3 className="text-2xl font-semibold uppercase">Servicios</h3>
+                </BorderLeft>
+                <div className="grid gap-3">
+                  {serviciosOfrecidos.map((servicio) => {
+                    return (
+                      <article
+                        key={servicio.id}
+                        id={`servicio-${servicio.id}`}
+                        className="flex gap-4 items-center justify-start">
+                        <div className="w-8 h-8">
+                          <FaCheck className="text-secondary" size={28} />
+                        </div>
+                        <span className="text-lg">{servicio.title}</span>
+                      </article>
+                    );
+                  })}
+                </div>
+                <ButtonContainer position="items-center justify-start" distance="h-20">
+                  <Button
+                    title="Contáctanos"
+                    href="/contacto"
+                    width="w-full sm:w-[260px] scale-[0.8]"
+                    variant="secondary"
+                  />
+                </ButtonContainer>
+              </div>
+              <img
+                src={image}
+                alt="vista interna de lavandería Easylav con persona"
+                title="lavandería Easylav"
+                className="shadow-image"
+                loading="lazy"
+              />
+            </TwoColumnsContainer>
+          </Card>
+          {/* Servicios */}
+        </div>
       </Container>
     </Section>
   );
