8f8249e95e729702620f38f83b91fd612c836529
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-18 12:44:50 -0600

    refactor: mejora la documentación y la estructura del componente Container

--- src/components/containers/Container.jsx
+++ src/components/containers/Container.jsx
@@ -1,28 +1,26 @@
 /*
-  Container - Component
+  Container.jsx - Componente contenedor para la aplicación
   =====================================
   Creado por : Daniel Pérez
   Fecha: 2024-08-28
   Modificado: 2025-01-24
+  Descripción: Este componente se utiliza como un contenedor principal para la aplicación, proporcionando un diseño responsivo y centrado. Se puede utilizar para envolver otros componentes o elementos de la interfaz de usuario. El contenedor tiene un ancho máximo definido para diferentes tamaños de pantalla, lo que garantiza que el contenido se vea bien en dispositivos móviles y de escritorio.
+  Uso: Este componente se puede importar y utilizar en cualquier parte de la aplicación donde se necesite un contenedor. Se puede personalizar el ID y la clase del contenedor según sea necesario. El contenido se pasa como hijos (children) del componente.
 */
 
 // Importaciones
 import PropTypes from 'prop-types';
 
-// Estilos
-const CONTAINER_UI = {
-  DISPLAY: 'md:max-w-screen-md lg:max-w-screen-md xl:max-w-screen-lg mx-auto',
-};
-// Estructura4
+// Estructura
 export default function Container({ id, children, className }) {
-  const classes = `${className} ${CONTAINER_UI.DISPLAY} `;
   return (
-    <article id={`container-${id}`} className={classes}>
+    <article id={`container-${id}`} className="md:max-w-screen-md lg:max-w-screen-md xl:max-w-screen-lg mx-auto">
       {children}
     </article>
   );
 }
 
+// Propiedades
 Container.propTypes = {
   id: PropTypes.string,
   className: PropTypes.string,
