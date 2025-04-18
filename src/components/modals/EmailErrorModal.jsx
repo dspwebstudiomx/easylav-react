ff40a5ad855563fd4d105248738c93feb75e4445
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-18 12:37:57 -0600

    refactor: agrega manejo de scroll en los modales de error y éxito al enviar correo

--- src/components/modals/EmailErrorModal.jsx
+++ src/components/modals/EmailErrorModal.jsx
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { IoMdExit } from 'react-icons/io';
 import Button from '../buttons/Button';
 import Modal from '../contacto/Modal';
 
 const EmailErrorModal = ({ onClick }) => {
+  useEffect(() => {
+    // Agregar clase para deshabilitar el scroll
+    document.body.classList.add('no-scroll');
+
+    // Limpiar la clase al desmontar el componente
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, []);
+
   return (
     <Modal>
       <div className="w-[40vw] bg-red_light text-red_dark text-xl p-12 xl:py-6 rounded-2xl border-2 border-red flex items-center justify-between gap-6">
@@ -25,6 +36,7 @@ const EmailErrorModal = ({ onClick }) => {
     </Modal>
   );
 };
+
 EmailErrorModal.propTypes = {
   onClick: PropTypes.func,
 };
--- src/components/modals/EmailSuccessModal.jsx
+++ src/components/modals/EmailSuccessModal.jsx
@@ -1,9 +1,20 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { IoMdExit } from 'react-icons/io';
 import Button from '../buttons/Button';
 import Modal from '../contacto/Modal';
 
 const EmailSuccessModal = ({ onClick }) => {
+  useEffect(() => {
+    // Agregar clase para deshabilitar el scroll
+    document.body.classList.add('no-scroll');
+
+    // Limpiar la clase al desmontar el componente
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, []);
+
   return (
     <Modal>
       <div className="bg-primary_light text-primary_dark text-xl p-12 py-12 xl:py-6 rounded-2xl border-2 border-primary flex gap-12 items-center justify-between">
@@ -24,6 +35,7 @@ const EmailSuccessModal = ({ onClick }) => {
     </Modal>
   );
 };
+
 EmailSuccessModal.propTypes = {
   onClick: PropTypes.func,
 };
