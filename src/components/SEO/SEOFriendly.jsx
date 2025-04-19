ff20dbc47f383fa4b903d2677b91e6809760f3e9
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-18 20:38:48 -0600

    feat: agrega soporte para Google Tag Manager en el componente SEOFriendly

--- src/components/SEO/SEOFriendly.jsx
+++ src/components/SEO/SEOFriendly.jsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 // Acceso a las variables de entorno
 const googleAnalyticsId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID || '';
 const googleAdsId = import.meta.env.VITE_GOOGLE_ADS_ID || '';
+const googleTagManagerId = import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID || '';
 
 const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogImageAlt, ogType, publisher }) => {
   return (
@@ -60,6 +61,17 @@ const SEOFriendly = ({ title, description, author, keywords, url, ogImage, ogIma
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${googleAnalyticsId}');
+        `}
+      </script>
+
+      {/* Google Tag Manager */}
+      <script>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${googleTagManagerId}');    
         `}
       </script>
 
