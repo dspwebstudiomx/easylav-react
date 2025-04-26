612369afe9a47a982553d55b30d5754386d25ac2
dspwebstudiomx <dspwebstudio@gmail.com>
2025-04-25 23:17:44 -0600

    feat: ajusta la clase de la cuadrícula en TwoColumnsContainer para mejorar la responsividad

--- src/components/containers/TwoColumnsContainer.jsx
+++ src/components/containers/TwoColumnsContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // Estructura
 const TwoColumnsContainer = ({ children }) => {
-  return <div className="grid md:grid-cols-2 gap-6 sm:gap-12 justify-between items-center">{children}</div>;
+  return <div className="grid lg:grid-cols-2 gap-6 sm:gap-12 justify-between items-center">{children}</div>;
 };
 export default TwoColumnsContainer;
 
