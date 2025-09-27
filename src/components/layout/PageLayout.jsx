9fd1d30cd4e18d9c9243ee5b4206f600dce7eef9
dspwebstudiomx <dspwebstudio@gmail.com>
2025-09-27 13:47:26 -0600

    feat: ajusta la altura mínima en el componente PageLayout para mejorar la presentación del contenido

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -20,7 +20,7 @@ const PageLayout = ({ children, id }) => {
   return (
     <main
       id={id}
-      className="w-full h-full dark:bg-dark mt-0 md:mt-[60px] xl:mt-0 min-h-[52.2vh] flex flex-col justify-center items-center dark:text-light">
+      className="w-full h-full dark:bg-dark mt-0 md:mt-[60px] xl:mt-0 min-h-[53.7vh] flex flex-col justify-center items-center dark:text-light">
       <Section>
         <Container className="">{children}</Container>
       </Section>
