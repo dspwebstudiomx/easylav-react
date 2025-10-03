f7d1e57606d0ba13635ff346014584406c0affa2
dspwebstudiomx <dspwebstudio@gmail.com>
2025-10-03 12:09:15 -0600

    feat: añade clase w-full al contenedor de cuatro columnas para asegurar el ancho completo

--- src/components/containers/FourColumnsContainer.jsx
+++ src/components/containers/FourColumnsContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // Estructura
 const FourColumnsContainer = ({ children }) => {
-  return <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8">{children}</div>;
+  return <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8 w-full">{children}</div>;
 };
 export default FourColumnsContainer;
 
