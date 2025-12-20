f004d5922fb404003dc3a34f6f7350efa813f8a9
dspwebstudiomx <dspwebstudio@gmail.com>
2025-12-20 11:43:31 -0600

    feat: añade el componente EliminacionCuentas a los términos y condiciones

--- src/pages/usos-beneficios/UsosBeneficios.jsx
+++ src/pages/usos-beneficios/UsosBeneficios.jsx
@@ -29,6 +29,7 @@ import { VigenciaPuntos } from './VigenciaPuntos';
 import { FallecimientoUsuario } from './FallecimientoUsuario';
 import { ReglasParticipacion } from './ReglasParticipacion';
 import { ReglasMonedero } from './ReglasMonedero';
+import { EliminacionCuentas } from './EliminacionCuentas';
 
 // Estructura
 const TerminosCondiciones = () => {
@@ -100,6 +101,7 @@ const TerminosCondiciones = () => {
             <FallecimientoUsuario />
             <ReglasParticipacion />
             <ReglasMonedero />
+            <EliminacionCuentas />
             {/* Introducción */}
           </ParagraphContainer>
           <h4 className="text-center w-full mt-12 text-lg">Última actualización: 18 de diciembre de 2025</h4>
