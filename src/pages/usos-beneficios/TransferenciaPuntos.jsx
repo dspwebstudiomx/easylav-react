84bb88e2b5ef80b0c712c8b8371e476ea70a3172
dspwebstudiomx <dspwebstudio@gmail.com>
2025-12-20 11:46:54 -0600

    feat: añade el componente TransferenciaPuntos para gestionar la transferencia de puntos

---
+++ src/pages/usos-beneficios/TransferenciaPuntos.jsx
@@ -0,0 +1,19 @@
+import { Paragraph, ParagraphContainer } from 'components';
+
+export const TransferenciaPuntos = () => {
+  return (
+    <div className="mt-12">
+      <ParagraphContainer>
+        <Paragraph>
+          <h2 className="font-semibold text-secondary_dark dark:text-secondary_light">13. TRANSFERENCIA DE PUNTOS.</h2>
+        </Paragraph>
+        <div className="flex flex-col gap-8 ml-6">
+          <Paragraph>
+            13.1. La mecánica de transferencia de Puntos NO ES APLICABLE a otros Usuario del Programa de “Tarjeta de
+            recompensa”.
+          </Paragraph>
+        </div>
+      </ParagraphContainer>
+    </div>
+  );
+};
