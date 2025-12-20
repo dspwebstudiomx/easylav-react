6ebf5b9e2ffdd5f460e8a67de7f733d33df63161
dspwebstudiomx <dspwebstudio@gmail.com>
2025-12-20 12:08:58 -0600

    feat: integra nuevos componentes para mejorar la presentación de términos y condiciones

--- src/pages/usos-beneficios/UsosBeneficios.jsx
+++ src/pages/usos-beneficios/UsosBeneficios.jsx
@@ -15,8 +15,6 @@ import {
   SEOFriendly,
   TitleH1,
   // TituloYDescripcion,
-  Paragraph,
-  ParagraphContainer,
 } from 'components';
 import { Glosario } from './Glosario';
 import { RegistroPrograma } from './RegistroPrograma';
@@ -32,6 +30,9 @@ import { ReglasMonedero } from './ReglasMonedero';
 import { EliminacionCuentas } from './EliminacionCuentas';
 import { TransferenciaPuntos } from './TransferenciaPuntos';
 import { ReposicionTarjeta } from './ReposicionTarjeta';
+import { IntroduccionTerminosCondiciones } from './IntroduccionTerminosCondiciones';
+import { ObligacionesUsuario } from './ObligacionesUsuario';
+import { BajaVoluntaria } from './BajaVoluntaria';
 
 // Estructura
 const TerminosCondiciones = () => {
@@ -56,58 +57,23 @@ const TerminosCondiciones = () => {
         </BorderLeft>
         {/* Columna 1 */}
         <article id="terminos-condiciones" className={'mt-12 text-lg'}>
-          <ParagraphContainer>
-            {/* Introducción */}
-            <Paragraph>
-              Programa de recompensa de lealtad desarrollado y operado por EASYLAV® con domicilio en Laguna de Parras,
-              585 – A Colonia Ventura Puente, Morelia, Michoacán, C. P. 58020. Los presentes términos y condiciones
-              generales tienen como objetivo establecer las reglas, términos y condiciones de participación, uso y
-              aprovechamiento y restricciones del Programa de lealtad.
-            </Paragraph>
-            <Paragraph>
-              Hacemos de su conocimiento que, al registrar su “Tarjeta de Recompensas”, usted está de acuerdo con
-              participar en el mismo y, por ende, se entiende que usted ha leído estos Términos y Condiciones que rigen
-              su uso, manifestando así estar enterado y en aceptación de los mismos. Si el Usuario no acepta los
-              presentes Términos y Condiciones, deberá abstenerse de participar en el Programa de “Tarjeta de
-              Recompensa” a través de su tarjeta física. Por lo anterior, le solicitamos de la manera más atenta revise
-              este documento detenidamente.
-            </Paragraph>
-            <Paragraph>
-              El programa de “Tarjeta de Recompensa” podrá realizar libremente, en cualquier momento y sin necesidad de
-              previo aviso, modificaciones formales, procedimentales o sustanciales a los presentes Términos y
-              Condiciones, estas incluyen, pero no se limitan a realizar correcciones, adiciones, mejoras o
-              modificaciones al contenido, presentación, información, bases de datos y demás elementos del Programa de
-              lealtad, por mencionar algunos (en adelante las “Actualizaciones”), siendo aplicables los Términos y
-              Condiciones publicados en el sitio web{' '}
-              <a
-                className="font-semibold text-secondary_dark dark:text-secondary"
-                href="/terminos-condiciones"
-                target="_blank">
-                www.easylav.mx/terminos-condiciones
-              </a>
-            </Paragraph>
-            <Paragraph>
-              Usted está de acuerdo que aceptará las Actualizaciones, siempre que continúe participando en el Programa
-              “Tarjeta de Recompensas”, por lo que no tendrá lugar ni derecho a ninguna reclamación o indemnización, ni
-              que lo mismo implique reconocimiento de responsabilidad alguna a favor del Usuario.
-            </Paragraph>
-            <Paragraph></Paragraph>
-            <Glosario />
-            <RegistroPrograma />
-            <Cuenta />
-            <UsoTarjetaFisica />
-            <CentroAtencion />
-            <MediosComunicacion />
-            <Mecanicas />
-            <VigenciaPuntos />
-            <FallecimientoUsuario />
-            <ReglasParticipacion />
-            <ReglasMonedero />
-            <EliminacionCuentas />
-            <TransferenciaPuntos />
-            <ReposicionTarjeta />
-            {/* Introducción */}
-          </ParagraphContainer>
+          <IntroduccionTerminosCondiciones />
+          <Glosario />
+          <RegistroPrograma />
+          <Cuenta />
+          <UsoTarjetaFisica />
+          <CentroAtencion />
+          <MediosComunicacion />
+          <Mecanicas />
+          <VigenciaPuntos />
+          <FallecimientoUsuario />
+          <ReglasParticipacion />
+          <ReglasMonedero />
+          <EliminacionCuentas />
+          <TransferenciaPuntos />
+          <ReposicionTarjeta />
+          <ObligacionesUsuario />
+          <BajaVoluntaria />
           <h4 className="text-center w-full mt-12 text-lg">Última actualización: 18 de diciembre de 2025</h4>
         </article>
         {/* Columna 1 */}
