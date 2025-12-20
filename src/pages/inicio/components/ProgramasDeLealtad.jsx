70ae263b66a152819fbe98a07e4b37659a8eb1be
dspwebstudiomx <dspwebstudio@gmail.com>
2025-12-20 12:39:38 -0600

    feat: ajusta el tamaño de fuente en el componente ProgramasDeLealtad para mejorar la legibilidad

--- src/pages/inicio/components/ProgramasDeLealtad.jsx
+++ src/pages/inicio/components/ProgramasDeLealtad.jsx
@@ -49,7 +49,7 @@ const ProgramasDeLealtad = () => {
                 <span className="text-secondary_dark dark:text-secondary_light text-3xl md:text-5xl font-bold block">
                   ¡Tu lealtad tiene recompensas!
                 </span>
-                <span className="text-3xl">
+                <span className="text-xl md:text-2xl lg:text-3xl">
                   <strong>EASYLAV® </strong>
                   recompensa tu lealtad y te regala tu “Tarjeta de Recompensas” para premiar tu preferencia y consumos
                   con puntos y beneficios especialmente para ti, Solo usa tu tarjeta física.
