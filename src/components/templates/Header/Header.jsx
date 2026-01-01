330bc2cbdad9e0d75ccb9ac5ec5b7b07defc0944
dspwebstudiomx <dspwebstudio@gmail.com>
2026-01-01 14:49:37 -0600

    refactor: agrega el componente Snowfall al encabezado para mejorar la experiencia visual

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -1,8 +1,10 @@
 import { Navbar } from 'components';
+import Snowfall from '../../Snowfall';
 
 const Header = () => {
   return (
     <header id="header" className="relative w-full h-[100px] z-[1000]">
+      <Snowfall />
       <Navbar />
     </header>
   );
