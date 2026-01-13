27b95a96d2e039f1bbd2b2e969d73826e6170a50
dspwebstudiomx <dspwebstudio@gmail.com>
2026-01-13 12:01:42 -0600

    feat: elimina el enlace del componente BackgroundCard para mejorar la estructura

--- src/components/cards/BackgroundCard.jsx
+++ src/components/cards/BackgroundCard.jsx
@@ -8,32 +8,29 @@
 // Importaciones
 import BackgroundImageSection from 'components/sections/BackgroundImageSection';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 // Estructura
 const BackgroundCard = ({ id, title, image, href, rounded, height, width, className, alt }) => {
   return (
-    <Link to={href} id={`sucursal-${title}`} className={`shadow-2xl rounded-lg ${width}`}>
-      <BackgroundImageSection
-        id={id}
-        image_768={image}
-        image_576={image}
-        image_240={image}
-        image_1024={image}
-        image_1200={image}
-        image_1920={image}
-        opacity="opacity-30 hover:opacity-40"
-        backgroundColor="bg-dark"
-        rounded={rounded}
-        height={height}
-        width={width}
-        title={`Ciudad de ${title}`}
-        alt={alt}
-        to={href}
-        className={`flex justify-center items-center ${className}`}>
-        <h2 className="text-light uppercase text-2xl font-semibold tracking-wide">{title}</h2>
-      </BackgroundImageSection>
-    </Link>
+    <BackgroundImageSection
+      id={id}
+      image_768={image}
+      image_576={image}
+      image_240={image}
+      image_1024={image}
+      image_1200={image}
+      image_1920={image}
+      opacity="opacity-30 hover:opacity-40"
+      backgroundColor="bg-dark"
+      rounded={rounded}
+      height={height}
+      width={width}
+      title={`Ciudad de ${title}`}
+      alt={alt}
+      to={href}
+      className={`flex justify-center items-center shadow-2xl rounded-lg ${className} ${width}`}>
+      <h2 className="text-light uppercase text-2xl font-semibold tracking-wide">{title}</h2>
+    </BackgroundImageSection>
   );
 };
 BackgroundCard.propTypes = {
